7c71f3492caf6329bbf6142f9221c32a18f47db3
sly <ololo@ololo.org>
2007-02-14 17:42:05 +0000

    unused comments removed

--- unreal/src/repo/diagramexplorermodel.cpp
+++ unreal/src/repo/diagramexplorermodel.cpp
@@ -34,7 +34,6 @@ dbg;
     diagrams->insert("diagram", q);
   
     q1.exec("select name from diagram where type='diagrams'");
-    //qDebug() << q1.executedQuery();
     int nameClmn = q1.record().indexOf("name");
     while(q1.next()){           // fetching diagram names
         QString tableName = q1.value(nameClmn).toString();    
@@ -43,13 +42,11 @@ dbg;
         q2.prepare("select name from " + tableName);
         diagrams->insert(tableName, q2);
         q2.exec("select * from " + tableName + " order by id");
-        //  qDebug() << q2.executedQuery();
         int nameCol = q2.record().indexOf("name");
         int typeCol = q2.record().indexOf("type");  
         while(q2.next()){
             QString valueName = q2.value(nameCol).toString();
             QString typeName  = q2.value(typeCol).toString();
-            //   qDebug() << valueName << typeName;
             value = new TreeItem(valueName, typeName, tableName, diagrams, table);
             table->addChild(value);
         }    
@@ -70,22 +67,8 @@ bool  DiagramExplorerModel::setData(const QModelIndex& index, const QVariant &va
     QSqlQuery q1;
     QSqlQuery q2;
     q1.exec("update " +item->getDiagramName()+ " set name='"+ value.toString() + "' where name='" + item->getName() + "'");
-    
-//    qDebug() << item->parent()->name << " -- " << q1.executedQuery(); 
-//    q2.exec("select name from " + item->parent()->name); 
-//    while(q2.next()) qDebug() << q2.value(0).toString();
-
     q1.exec("update " + item->getType() + " set name='" + value.toString() + "' where name='" + item->getName() + "'");
-    
- //   qDebug() << item->type << " -- " << q1.executedQuery(); 
-//    q2.exec("select name from " + item->type); 
- //   qDebug() << item->type << " -- " << q2.lastQuery() ;
-//    while(q2.next()) qDebug() << q2.value(0).toString();
-
     item->setData(value.toString());
-    
-    //qDebug() << q.executedQuery(); q.exec("select name from " + item->parent()->name); 
-    //while(q.next()) qDebug() << q.value(0).toString();
     
     emit dataChanged(index, index);
     return true;
@@ -127,7 +110,6 @@ QVariant DiagramExplorerModel::data(const QModelIndex &index, int role) const{
  if (role != Qt::DisplayRole)
    return QVariant();
  TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
- //qDebug() << "== " << __FUNCTION__ << ": " << item->data(index.column());  
 
  return item->data();
 }
@@ -145,9 +127,6 @@ Qt::ItemFlags DiagramExplorerModel::flags(const QModelIndex &index) const{
 
 QVariant DiagramExplorerModel::headerData(int , Qt::Orientation , int ) const {
 //dbg;                               
-// if (orientation == Qt::Horizontal && role == Qt::DisplayRole)
-//   return rootItem->data(section);
- //return QVariant();
  return "";
 }
 
@@ -161,9 +140,7 @@ QModelIndex DiagramExplorerModel::index(int row, int column, const QModelIndex &
    parentItem = static_cast<TreeItem*>(parent.internalPointer());
  TreeItem *childItem = parentItem->getChild(row);
  
-// qDebug() << "index: row " << row;
  if (childItem){
-  // qDebug() << "index: row " << row << ", column " << column; 
    return createIndex(row, column, childItem);
  }  
  else
@@ -191,14 +168,12 @@ int DiagramExplorerModel::rowCount(const QModelIndex &parent) const{
     parentItem = static_cast<TreeItem*>(parent.internalPointer());
 
   }  
-  //qDebug() << "==" << __FUNCTION__ << ": " << parentItem->rowCount() << ", name = " << parentItem->getName();  
   return parentItem->rowCount();
 }
 
 void DiagramExplorerModel::updateData(const QModelIndex& index, QVariant value){
 //dbg;
   TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
-  //qDebug() << item->getDiagramName() << item->getName() << value.toString();
   rootItem->getChild(item->getDiagramName())->getChild(item->getName())->setData(value.toString());
 }
 
@@ -218,19 +193,7 @@ void DiagramExplorerModel::createDiagram(QString &dname){
     //curID++;
     tmp = "create table %1 (id integer primary key auto_increment, name varchar(20), type varchar(20))";
     q.exec(tmp.arg(dname));
-
-    //qDebug() << q.executedQuery(); q.exec("select name from diagram where type='diagrams'"); 
-    //while(q.next()) qDebug() << q.value(0).toString();
-
-//qDebug() << "rowcount: " << rootItem->rowCount();
-
-//    TreeItem *item = new TreeItem(dname, "diagram", "diagram", diagrams, rootItem);                  
-//    rootItem->addChild(item);       
-   
-//    q.prepare("select name from " + dname);
-  //  qDebug() << "select name from " + dname;
-//    diagrams->insert(dname, q);          
-
+    
     rootItem = new TreeItem("diagram", "diagram", "", diagrams, 0);   
     rescan();
 }
@@ -241,19 +204,10 @@ dbg;
 
     QString tmp = "insert into %1 (name, type) values ('%2', '%3')";
     q.exec(tmp.arg(values.at(0)).arg(values.at(1)).arg(values.at(6)));
-    //q.exec("select name from " + values.at(0)); qDebug() << q.executedQuery() << ": "; 
-    //while(q.next()) qDebug() << q.value(0).toString();
-
-
     tmp = "insert into %1 (%2) values ('%3', '%4', %5, '%6', '%7', '%8')";
-    //qDebug() << "list: " << values;
     tmp = tmp.arg(values.at(6)).arg(fields).arg(values.at(1)).arg(values.at(2)).arg(values.at(3)).arg(values.at(4)).arg(values.at(5)).arg(values.at(0));
-    //qDebug() << "tmp:" << tmp;
     q1.exec(tmp);
-//    qDebug() << q1.executedQuery();
- //   q1.exec("select name from nFeatured"); qDebug() << q1.executedQuery() << ": "; 
- //   while(q1.next()) qDebug() << q1.value(0).toString();
-   
+    
     rootItem = new TreeItem("diagram", "diagram", "", diagrams, 0);   
     rescan(); 
     
@@ -263,6 +217,6 @@ dbg;
      }
     
     emit elemAdded();
-
-   
 }
+
+
