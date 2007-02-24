a192f3665ef07acf6dfed41719678a2036d881c0
sly <ololo@ololo.org>
2007-02-24 21:42:48 +0000

    ok, NOW we have property editor files under control :)

---
+++ unreal/src/repo/propertyeditormodel.cpp
@@ -0,0 +1,181 @@
+//=====================================================================
+// Project:      unREAL 
+// File Name:    propertyeditormodel.cpp 
+// Description:  Model for Property Editor support
+//
+// Created:      24-Feb-07
+// Revision:     25-Feb-07 
+//
+// Author:       Timofey A. Bryksin (sly@tercom.ru)
+//===================================================================== 
+
+//#include <QtGui>
+//#define _LONG_DEBUG
+#include "propertyeditormodel.h"
+#include "dbg.h"
+
+
+PropertyEditorModel::PropertyEditorModel(QSqlDatabase &_db, DiagramExplorerModel *_dem, QObject *parent) : QAbstractTableModel(parent) {
+dbg;
+    dem = _dem;
+    db = _db;
+    diagram = "";
+    name = "";
+    rootd = static_cast<TreeItem*>(dem->index(0,0,QModelIndex()).internalPointer()); 
+    rootd = rootd->parent();
+}
+
+void PropertyEditorModel::rescan(const QModelIndex &index ){
+dbg;
+    
+    current = index;
+    TreeItem *ti = static_cast<TreeItem*>(index.internalPointer());
+
+    diagram = ti->getDiagramName();
+    type    = ti->getType();
+    name    = ti->getName();
+
+    QSqlQuery q;  
+    QString tmp = "select * from %1 where name='%2' and diagram='%3'";
+    tmp = tmp.arg(type).arg(name).arg(diagram);
+    
+    q = db.exec(tmp);
+    QSqlRecord r = q.record();
+
+    while(q.next()){ 
+        fields.clear(); 
+        vals.clear();
+        for( int i = 0; i < r.count(); i++ ){
+            fields << r.fieldName(i);
+            vals   << q.value(i).toString();
+        }
+    }
+}
+
+void PropertyEditorModel::adjustData(const QModelIndex &index ){
+dbg;    
+    rescan( index );
+}
+
+
+void PropertyEditorModel::updateName( QStringList list ){
+dbg;
+    
+    vals.replace(fields.indexOf("name"), list.at(1));
+    QModelIndex ind = createIndex(fields.indexOf("name"), 1, index(fields.indexOf("name"),1,QModelIndex()).internalPointer());
+    emit dataChanged(ind, ind);
+}
+
+
+PropertyEditorModel::~PropertyEditorModel(){
+dbg;    
+}
+
+bool PropertyEditorModel::setData(const QModelIndex& index, const QVariant &value, int role){
+dbg;
+    if (index.isValid() && role == Qt::EditRole) {
+        
+        if( elementExists(value.toString(), type, diagram) <= 0)
+            return false;
+        
+        QString field  = fields.at(index.row());
+        QString oldval = vals.at(index.row());
+        
+        QString tmp = "update %1 set %2='%3' where %2='%4' and diagram='%5'";
+        QString tmp2 = tmp.arg(type).arg(field).arg(value.toString()).arg(oldval).arg(diagram);
+        db.exec(tmp2);
+
+        if( field == "name" ){
+            tmp2 = tmp.arg(diagram).arg(field).arg(value.toString()).arg(oldval);
+            db.exec(tmp2);
+            name = value.toString();
+            QStringList list;
+            list << oldval << value.toString() << diagram << type;
+            emit nameChanged( list );
+        }
+
+        if( field == "diagram"){
+        
+        }
+        
+        vals.replace(index.row(), value.toString());
+       
+        emit dataChanged(index, index);
+        return true;
+    }
+    return false;
+}
+
+int PropertyEditorModel::columnCount(const QModelIndex &) const{
+dbg;
+    return 2;
+}
+
+QVariant PropertyEditorModel::data(const QModelIndex &index, int role) const{
+//dbg;
+    if (!index.isValid())
+        return QVariant();
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    int col = index.column();
+    int row = index.row();
+    QString res;
+   
+    if (col == 0)
+        res = fields.at(row);
+    else if (col == 1)
+        res = vals.at(row);
+    return res;
+}
+
+Qt::ItemFlags PropertyEditorModel::flags(const QModelIndex &index) const{
+dbg;
+    Qt::ItemFlags f = Qt::ItemIsEnabled;
+    if (!index.isValid())
+        return f;
+    if (index.column() == 1)
+        f |= Qt::ItemIsEditable;
+    return f | Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+}
+
+QVariant PropertyEditorModel::headerData(int , Qt::Orientation , int ) const {
+dbg;                               
+    return "";
+}
+
+/*
+QModelIndex PropertyEditorModel::index(int row, int column, const QModelIndex &parent) const{
+dbg;            
+    return createIndex(row, column, QModelIndex());
+}
+
+QModelIndex PropertyEditorModel::parent(const QModelIndex &index) const{
+dbg;
+    return QModelIndex();
+}
+*/
+
+int PropertyEditorModel::rowCount(const QModelIndex &) const{
+dbg;
+    return vals.size();
+}
+
+int PropertyEditorModel::elementExists( QString name, QString , QString diagram){
+dbg;
+    TreeItem* par = rootd->getChild(diagram);
+    qDebug() << diagram;
+    qDebug() << par;
+    qDebug() << par->getName();
+    if (!par){
+        QMessageBox::critical(0, QObject::tr("error"), QObject::tr("requested diagram not found.\nyou should create diagram first"));
+        return -1;
+    }   
+    TreeItem *child = par->getChild(name);
+    if(child){
+        QMessageBox::critical(0, QObject::tr("error"), QObject::tr("element with such name already exists.\nchoose another name plz"));
+        return 0;
+    }
+    return 1;
+}
+
---
+++ unreal/src/repo/propertyeditormodel.h
@@ -0,0 +1,65 @@
+//=====================================================================
+// Project:      unREAL 
+// File Name:    propertyeditormodel.h 
+// Description:  Model for Property Editor support
+//
+// Created:      24-Feb-07
+// Revision:     25-Feb-07 
+//
+// Author:       Timofey A. Bryksin (sly@tercom.ru)
+//===================================================================== 
+
+#ifndef NEW
+#define NEW
+
+#include <QtGui>
+#include <QAbstractTableModel>
+
+#include "treeitem.h"
+#include "diagramexplorermodel.h"
+#include "objectexplorermodel.h"
+
+class TreeItem;
+
+class PropertyEditorModel : public QAbstractTableModel
+{
+    Q_OBJECT
+
+public:
+    PropertyEditorModel(QSqlDatabase&, DiagramExplorerModel*, QObject *parent = 0);
+    ~PropertyEditorModel();
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole); 
+    QVariant data(const QModelIndex &index, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    //QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    ///QModelIndex parent(const QModelIndex &index) const;
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    
+    int elementExists( QString, QString, QString );    
+ 
+signals:
+    void nameChanged( QStringList );
+
+public slots:
+    void adjustData( const QModelIndex &);
+    void updateName( QStringList );
+    
+private:          
+    QModelIndex current;
+    DiagramExplorerModel *dem;
+    TreeItem *rootd;
+    void rescan( const QModelIndex& ); // DO NOT USE IT OUTSIDE THE CONSTRUCTOR!! YOU GONNA BURN IN HELL FOR THAT!
+    QSqlDatabase db;
+    QStringList fields;
+    QStringList vals;
+    QString diagram;
+    QString name;
+    QString type;
+};
+
+
+#endif
+
