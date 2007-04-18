41db57fe4c1c6e1f374aaca508f5be9148a2665a
ixoth <ololo@ololo.org>
2007-04-18 12:17:00 +0000

    Added arrow support

--- trunk/umllib/uml_edgeelement.h
+++ trunk/umllib/uml_edgeelement.h
@@ -6,39 +6,43 @@
 QPainterPath qt_graphicsItem_shapeFromPath(const QPainterPath &path, const QPen &pen);
 
 namespace UML {
-    class NodeElement;
-    class EdgeElement : public Element
-    {
-    public:
-        EdgeElement();
-	~EdgeElement();
+	class NodeElement;
+	class EdgeElement : public Element
+	{
+		public:
+			EdgeElement();
+			~EdgeElement();
 
-        void updateData();
+			void updateData();
 
-	QRectF boundingRect() const;
-	QPainterPath shape() const;
-	void paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*);
+			QRectF boundingRect() const;
+			QPainterPath shape() const;
+			void paint(QPainter*, const QStyleOptionGraphicsItem*, QWidget*);
 
-	void adjustLink();
-	void removeLink(UML::NodeElement *from) { if (src == from) { src = 0; }; if (dst == from) { dst = 0; }; };
+			void adjustLink();
+			void removeLink(UML::NodeElement *from) { if (src == from) { src = 0; }; if (dst == from) { dst = 0; }; };
 
-    protected:
-        void mousePressEvent ( QGraphicsSceneMouseEvent * event );
-        void mouseMoveEvent ( QGraphicsSceneMouseEvent * event );
-        void mouseReleaseEvent ( QGraphicsSceneMouseEvent * event );
+		protected:
+			void mousePressEvent ( QGraphicsSceneMouseEvent * event );
+			void mouseMoveEvent ( QGraphicsSceneMouseEvent * event );
+			void mouseReleaseEvent ( QGraphicsSceneMouseEvent * event );
 
-	void contextMenuEvent ( QGraphicsSceneContextMenuEvent * event );
+			void contextMenuEvent ( QGraphicsSceneContextMenuEvent * event );
 
-    private:
-	NodeElement *src, *dst;
-//	QPointF srcPoint, dstPoint;
-    
-	int dragState;
-	
-	int portFrom, portTo;
 
-	QPolygonF m_line;
-    };
+			virtual void drawStartArrow ( QPainter * ) = 0;
+			virtual void drawEndArrow ( QPainter * ) = 0;
+
+		private:
+			NodeElement *src, *dst;
+			//	QPointF srcPoint, dstPoint;
+
+			int dragState;
+
+			int portFrom, portTo;
+
+			QPolygonF m_line;
+	};
 };
 
 #endif
