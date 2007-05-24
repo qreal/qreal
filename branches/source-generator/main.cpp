ce35714f06b011db517fdb61a346532864a04e2d
sly <ololo@ololo.org>
2007-05-24 09:25:27 +0000

    1. more fixes 2. new metamodels

--- branches/source-generator/main.cpp
+++ branches/source-generator/main.cpp
@@ -3,15 +3,15 @@
 int main()
 {
     QStringList l;
-    l   << "kernel_metamodel.xml" 
+    l
+       << "kernel_metamodel.xml" 
         << "basicbehaviors_metamodel.xml" 
         << "trace.xml"
         << "requirements_metamodel.xml" 
         << "class_metamodel.xml"
-//        << "class_metamodel.xml"
-        << "usecase_metamodel.xml";
-    //    << "sequence_metamodel.xml"
-      //  << "communication_metamodel.xml";
+        << "usecase_metamodel.xml"
+        << "sequence_metamodel.xml"
+        << "communication_metamodel.xml";
     new Generator(l);
     return 0;
 }
