c32bb249cbaf6fc443c5ae0af4a6aee3ad260266
yurii <ololo@ololo.org>
2008-01-24 12:29:37 +0000

    Stub for PADL generator

---
+++ branches/new-repo-experimenting/GenPADL/GenPADL.cpp
@@ -0,0 +1,15 @@
+#include "GenPADL.h"
+
+#include <QtGui>
+
+using namespace GenPADL;
+
+void PADLGenerator::Generate() const
+{
+  QMessageBox MessageBox(QMessageBox::Information
+    , "Something works..."
+    , "Dummy generator called successfully."
+    , QMessageBox::Ok
+  );
+  MessageBox.exec();
+} 
---
+++ branches/new-repo-experimenting/GenPADL/GenPADL.h
@@ -0,0 +1,15 @@
+#ifndef GENPADL_H
+#define GENPADL_H
+
+namespace GenPADL
+{
+
+  class PADLGenerator
+  {
+  public:
+    void Generate() const;
+  };
+
+}
+
+#endif
