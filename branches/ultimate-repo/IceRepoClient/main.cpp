244e5dd050dda9d02d9fe38ed21b2bdffed55760
seppel <ololo@ololo.org>
2008-04-19 13:14:49 +0000

    buggy commit

--- branches/ultimate-repo/IceRepoClient/main.cpp
+++ branches/ultimate-repo/IceRepoClient/main.cpp
@@ -8,9 +8,18 @@
 int main()
 {
 	QTextStream out(stdout);
-	RealRepoClient realRepoClient;
-	out << "Connected to repo\n";
-	sleep(2);
+	RealRepoClient repoClient;
+
+	out << "getTypesCount: " << repoClient.getTypesCount() << endl;
+	QIntList typesList = repoClient.getAllTypes();
+	out << "count: " << typesList.count() << endl;
+	for (QIntList::const_iterator i = typesList.begin(); i != typesList.end(); i++)
+	{
+		RealType* realType = repoClient.getTypeById(*i);
+		//out << "Type N" << *i << " has name: " << realType->getName() << endl;
+		delete realType;
+	}
+	
 	return 0;
 }
 
