cad97c9d44899d8b4472340934fdee9b056ab0ff
yurii <ololo@ololo.org>
2008-06-16 21:40:45 +0000

    Some refactoring, experimental saving.

--- branches/ultimate-repo/reposerver/repothread.h
+++ branches/ultimate-repo/reposerver/repothread.h
@@ -9,6 +9,8 @@
 namespace reposerver
 {
   
+  typedef QVector<QString> QStringVector;
+  
   class QRealRepoServerThread: public QThread
   {
     Q_OBJECT
@@ -28,14 +30,45 @@ namespace reposerver
     void error(QTcpSocket::SocketError socketError);
     
   private:
-    void handleCommand(QString const &cmd, QTcpSocket *const socket);
-    int getId();
+    QString handleCommand(QString const &cmd);
+    
+    QString handleGetName(QStringVector const &params);
+    QString handleSetName(QStringVector const &params);
+    QString handleSetParent(QStringVector const &params);
+    QString handleCreateEntity(QStringVector const &params);    
+    QString handleDeleteEntity(QStringVector const &params);
+    QString handleGetTypesCount(QStringVector const &params);
+    QString handleGetAllTypes(QStringVector const &params);
+    QString handleGetTypeInfo(QStringVector const &params);
+    QString handleGetTypeByName(QStringVector const &params);
+    QString handleGetTypesByMetatype(QStringVector const &params);
+    QString handleGetObjectsByType(QStringVector const &params);
+    QString handleGetObjectData(QStringVector const &params);
+    QString handleGetChildren(QStringVector const &params);
+    QString handleGetDescription(QStringVector const &params);
+    QString handleSetDescription(QStringVector const &params);
+    QString handleGetPosition(QStringVector const &params);
+    QString handleSetPosition(QStringVector const &params);
+    QString handleGetConfiguration(QStringVector const &params);
+    QString handleSetConfiguration(QStringVector const &params);
+    QString handleSetProperty(QStringVector const &params);
+    QString handleGetProperty(QStringVector const &params);
+    QString handleAddLink(QStringVector const &params);
+    QString handleRemoveLink(QStringVector const &params);
+    QString handleGetEntireObject(QStringVector const &params);
+    QString handleGetLinksByObject(QStringVector const &params);
+    QString handleGetObjectsByLink(QStringVector const &params);
+
+    bool IsParamsNumberCorrect(QStringVector const &params, QString const &command
+      , int const &paramsNum) const;
+    void TryToRestoreState();
     
     int mSocketDescriptor;
-    bool mCanRead;
     Root *mRoot;
     RepoTypesInfo *mTypesInfo;
     int mCounter;
+    
+    mutable QString mLog;
   };
   
 }
