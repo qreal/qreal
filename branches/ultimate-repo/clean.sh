3df8226a3b0e9417ded20e23d04c018debe3ed13
lich <ololo@ololo.org>
2008-06-16 22:42:49 +0000

    Do not stop cleaning process if some Makefiles are not found.

--- branches/ultimate-repo/clean.sh
+++ branches/ultimate-repo/clean.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cd ../source-generator && make clean && cd ../ultimate-repo && make clean && cd reposerver && make clean
+cd ../source-generator && make clean
+cd ../ultimate-repo && make clean
+cd reposerver && make clean
