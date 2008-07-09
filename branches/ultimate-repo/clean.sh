c8c26e349200ebd70ec5820379b340ed4a14a8ca
lich <ololo@ololo.org>
2008-07-09 13:26:28 +0000

    Clean .moc and .obj directories.

--- branches/ultimate-repo/clean.sh
+++ branches/ultimate-repo/clean.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-cd ../source-generator && make distclean
-cd ../ultimate-repo && make distclean
-cd reposerver && make distclean
+
+(cd ../source-generator && make distclean)
+(cd reposerver && make distclean)
+make distclean
+
+# This is a workaround of qt4 bug #181577: QMAKE_[DIST]CLEAN does not remove
+# directories
+rm -rf .moc
+rm -rf .obj
+rm -rf reposerver/.moc
+rm -rf reposerver/.obj
