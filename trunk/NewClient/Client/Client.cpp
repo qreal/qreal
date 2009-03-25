363374e27f2e296b542cfbc849b627cc6c7df5d0
seva <ololo@ololo.org>
2009-03-25 21:12:50 +0000

    Logical structure of new client improved

--- trunk/NewClient/Client/Client.cpp
+++ trunk/NewClient/Client/Client.cpp
@@ -36,7 +36,12 @@ IdTypeList Client::parents( IdType id )
 void Client::addParent( IdType id, IdType parent )
 {
 	if (mObjects.contains(id)) {
-		mObjects[id]->addParent(parent);
+		if (mObjects.contains(parent)) {	
+			mObjects[id]->addParent(parent);
+			mObjects[parent]->addChild(id);
+		} else {
+			throw Exception("Client: Adding nonexistent parent " + parent + " to  object " + id);
+		}
 	} else {
 		throw Exception("Client: Adding parent " + parent + " to nonexistent object " + id);
 	}
@@ -63,7 +68,12 @@ void Client::addChild( IdType id, IdType child )
 void Client::removeParent( IdType id, IdType parent )
 {
 	if (mObjects.contains(id)) {
-		mObjects[id]->removeParent(parent);
+		if (mObjects.contains(parent)) {
+			mObjects[id]->removeParent(parent);
+			mObjects[parent]->removeChild(id);
+		} else {
+			throw Exception("Client: Removing nonexistent parent " + parent + " from object " + id);
+		}
 	} else {
 		throw Exception("Client: Removing parent " + parent + " from nonexistent object " + id);
 	}
@@ -72,7 +82,21 @@ void Client::removeParent( IdType id, IdType parent )
 void Client::removeChild( IdType id, IdType child )
 {
 	if (mObjects.contains(id)) {
-		mObjects[id]->removeChild(child);
+		if (mObjects.contains(child)) {
+			mObjects[id]->removeChild(child);
+			if (mObjects[child]->parents().size()!=1) {
+				mObjects[child]->removeParent(id);
+			} else {
+				if (mObjects[child]->parents().first()==id) {
+					delete mObjects[child];
+					mObjects.remove(child);
+				} else {
+					throw Exception("Client: removing child " + child + " from object " + id + ", which is not his parent");
+				}
+			}
+		} else {
+		throw Exception("Client: removing nonexistent child " + child + " from object " + id);
+		}
 	} else {
 		throw Exception("Client: removing child " + child + " from nonexistent object " + id);
 	}
