edf5655cc10dff9d91b5e338d5894fc2823a47a2
seva <ololo@ololo.org>
2009-04-02 13:32:10 +0000

    Насяльника приказал ;)

--- trunk/NewClient/Model/Model.h
+++ trunk/NewClient/Model/Model.h
@@ -18,15 +18,15 @@ namespace qReal {
 		public:
 			Model();
 			~Model();
-			Qt::ItemFlags flags(const QModelIndex &index) const;
-			QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
-			QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-			int rowCount(const QModelIndex &parent = QModelIndex()) const;
-			int columnCount(const QModelIndex &parent = QModelIndex()) const;
-			bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole); 
-			bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());  
-			QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const; 
-			QModelIndex parent(const QModelIndex &index) const;
+			virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+			virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+			virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+			virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+			virtual int columnCount(const QModelIndex &parent = QModelIndex()) const;
+			virtual bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole); 
+			virtual bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());  
+			virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const; 
+			virtual QModelIndex parent(const QModelIndex &index) const;
 
 		private:
 			client::Client *mClient;
