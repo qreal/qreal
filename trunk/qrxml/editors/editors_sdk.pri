62eba2c24f72754d2f19a7ea0d71355fc79016ab
yurii <ololo@ololo.org>
2009-04-05 10:33:32 +0000

    экспериментальные sdf-ки, ещё парочка ненужных файлов.

---
+++ trunk/qrxml/editors/editors_sdk.pri
@@ -0,0 +1,11 @@
+SOURCES		+= \
+			../../qrgui/umllib/sdfrenderer.cpp \
+			../../qrgui/umllib/uml_nodeelement.cpp \
+			../../qrgui/umllib/uml_edgeelement.cpp \
+			../../qrgui/umllib/uml_element.cpp
+
+HEADERS		+= \
+			../../qrgui/umllib/sdfrenderer.h \
+			../../qrgui/umllib/uml_nodeelement.h \
+			../../qrgui/umllib/uml_edgeelement.h \
+			../../qrgui/umllib/uml_element.h
---
+++ trunk/qrxml/editors/kroki.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "../../qrgui/umllib/uml_nodeelement.h"
+
+namespace UML {
+
+	class Kroki : public NodeElement {
+	public:
+		Kroki() {
+			renderer.load(QString(":/TestEditor/Orthodox/Kroki.sdf"));
+			text = "";
+			height = -1;
+			width = -1;
+			m_contents.setWidth(width);
+			m_contents.setHeight(height);
+			d.setFlags(QGraphicsItem::ItemIsSelectable | d.flags());
+			d.setTextInteractionFlags(Qt::TextEditorInteraction);
+			d.setParentItem(this);
+		}
+
+		virtual ~Kroki() {}
+
+		void paint(QPainter *painter, const QStyleOptionGraphicsItem *style, QWidget *widget)
+		{
+			renderer.render(painter, m_contents);
+			d.setTextWidth(m_contents.width() - 15);
+		}
+
+		void updateData()
+		{
+			NodeElement::updateData();
+			text = "";
+			update();
+		}
+
+	private:
+		QString text;
+		int width;
+		int height;
+		SdfRenderer renderer;
+	};
+}
