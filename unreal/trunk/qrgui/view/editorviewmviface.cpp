80076dd9cb04e3b1686ed415566c85f270363e5e
yurii <ololo@ololo.org>
2010-01-31 21:59:06 +0000

    чо бывает

--- unreal/trunk/qrgui/view/editorviewmviface.cpp
+++ unreal/trunk/qrgui/view/editorviewmviface.cpp
@@ -218,7 +218,7 @@ EditorViewScene *EditorViewMViface::scene() const
 
 void EditorViewMViface::clearItems()
 {
-	foreach (QGraphicsItem *item, mItems)
+	foreach (QGraphicsItem *item, mItems.values())
 		delete item;
 	mItems.clear();
 }
