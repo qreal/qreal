0c88f3862b8e5ee1cffe38ba7e1dfd391532cf7b
seva <ololo@ololo.org>
2010-03-29 09:05:55 +0000

    qDebug to stdout. i hope it is compilable

--- unreal/trunk/qrxc/main.cpp
+++ unreal/trunk/qrxc/main.cpp
@@ -5,8 +5,27 @@
 #include <QDebug>
 #include "typelib.h"
 
+void myMessageOutput(QtMsgType type, const char *msg)
+ {
+	switch (type) {
+		case QtDebugMsg:
+			fprintf(stdout, "Debug: %s\n", msg);
+			break;
+		case QtWarningMsg:
+			fprintf(stderr, "Warning: %s\n", msg);
+			break;
+		case QtCriticalMsg:
+			fprintf(stderr, "Critical: %s\n", msg);
+			break;
+		case QtFatalMsg:
+			fprintf(stderr, "Fatal: %s\n", msg);
+			abort();
+	}
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMsgHandler(myMessageOutput);
 	QCoreApplication app(argc, argv);
 	QStringList args = app.arguments();
 
