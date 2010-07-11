35c8f849f5ace7fe1aade26596d7800227d5cd73
sly <ololo@ololo.org>
2010-07-11 16:00:35 +0000

    added forgotten file

---
+++ unreal/trunk/qrgui/umllib/sdfRendererInterface.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtGui/QPainter>
+
+class SdfRendererInterface : public QObject
+{
+public:
+	virtual bool load (const QString &filename) = 0;
+	virtual void render(QPainter *painter, const QRectF &bounds) = 0;
+};
+
+class SdfIconEngineV2Interface : public QIconEngineV2
+{
+public:
+};
