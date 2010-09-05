cf92e5fd5c1bfbffdce0c2b5648934e07fd070f4
seva <ololo@ololo.org>
2010-09-05 21:31:09 +0000

    installer version fix

--- unreal/trunk/installer/rplcver.bat
+++ unreal/trunk/installer/rplcver.bat
@@ -1,10 +1,15 @@
 @echo off
-setlocal EnableDelayedExpansion
 
-for /F "tokens=* delims==" %%A in (%1) do (
-	set STRING=%%A
-	set STRING=!STRING:0.0.0.0=%2!
-	echo !STRING!>> %1.tmp
+for /F "tokens=1* delims=]" %%j in ('type "%1" ^| find /V /N ""') do (
+	set STRING=%%k
+	setlocal EnableDelayedExpansion
+	if "!STRING!" == "" ( 
+		echo.>> %1.tmp
+	) else (
+		set STRING=!STRING:0.0.0.0=%2!
+		echo !STRING!>> %1.tmp
+	)
+	endlocal
 )
 
 move %1.tmp %1
