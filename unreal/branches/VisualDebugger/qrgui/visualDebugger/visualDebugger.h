5d4625949101a5299af6d5e9307699d386d2afd9
vladimir.polyakov <ololo@ololo.org>
2010-11-04 16:43:14 +0000

    Fixed according code style conventions.

--- unreal/branches/VisualDebugger/qrgui/visualDebugger/visualDebugger.h
+++ unreal/branches/VisualDebugger/qrgui/visualDebugger/visualDebugger.h
@@ -7,39 +7,39 @@
 #include "../view/editorview.h"
 
 namespace qReal {
-    class VisualDebugger : QObject
-    {
-	Q_OBJECT
-
-    public:
-	VisualDebugger(EditorView *editor, model::Model *model);
-	~VisualDebugger();
-    public slots:
-	void debug();
-	void debugSingleStep();
-    private:
-	enum ErrorType {
-	    missingBeginNode,
-	    missingEndOfLinkNode,
-	    endWithNotEndNode,
-	    missingValidLink,
-	    noErrors
+	class VisualDebugger : QObject
+	{
+		Q_OBJECT
+		
+	public:
+		VisualDebugger(EditorView *editor, model::Model *model);
+		~VisualDebugger();
+	public slots:
+		void debug();
+		void debugSingleStep();
+	private:
+		enum ErrorType {
+			missingBeginNode,
+			missingEndOfLinkNode,
+			endWithNotEndNode,
+			missingValidLink,
+			noErrors
+		};
+	private:
+		EditorView *mEditor;
+		model::Model *mModel;
+		UML::Element *mCurrentElem;
+		Id mCurrentId;
+		QGraphicsColorizeEffect *mEffect;
+		
+		void error(ErrorType e);
+		UML::Element* findBeginNode(QString name);
+		Id findValidLink();
+		void pause(int time);
+		bool isFinalNode(Id id);
+		bool hasEndOfLinkNode(Id id);
+		ErrorType doFirstStep(UML::Element* elem);
+		void doStep(Id id);
+		void deinitialize();
 	};
-    private:
-	EditorView *mEditor;
-	model::Model *mModel;
-	UML::Element *mCurrentElem;
-	Id mCurrentId;
-	QGraphicsColorizeEffect *mEffect;
-
-	void error(ErrorType e);
-	UML::Element* findBeginNode(QString name);
-	Id findValidLink();
-	void pause(int time);
-	bool isFinalNode(Id id);
-	bool hasEndOfLinkNode(Id id);
-	ErrorType doFirstStep(UML::Element* elem);
-	void doStep(Id id);
-	void deinitialize();
-    };
 }
