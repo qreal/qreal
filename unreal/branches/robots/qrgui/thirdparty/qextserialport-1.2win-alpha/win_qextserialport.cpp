978dc168ad0b01530fa390145fc54e19d69e7cab
yurii <ololo@ololo.org>
2011-02-07 15:15:55 +0000

    fix for qextserialport, allows to use COM ports with indexes more than 9.

--- unreal/branches/robots/qrgui/thirdparty/qextserialport-1.2win-alpha/win_qextserialport.cpp
+++ unreal/branches/robots/qrgui/thirdparty/qextserialport-1.2win-alpha/win_qextserialport.cpp
@@ -184,7 +184,7 @@ bool Win_QextSerialPort::open(OpenMode mode) {
         return isOpen();
     if (!isOpen()) {
         /*open the port*/
-        Win_Handle=CreateFileA(port.toAscii(), GENERIC_READ|GENERIC_WRITE,
+        Win_Handle=CreateFileA("\\\\.\\" + port.toAscii(), GENERIC_READ|GENERIC_WRITE,
                               FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, dwFlagsAndAttributes, NULL);
         if (Win_Handle!=INVALID_HANDLE_VALUE) {
             /*configure port settings*/
