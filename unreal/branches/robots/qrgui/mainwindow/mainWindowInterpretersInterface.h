3a5dc07c704e9bfa91fe7196289158681b07ef9d
sly <ololo@ololo.org>
2011-02-09 09:26:01 +0000

    make destructor virtual. ololo

--- unreal/branches/robots/qrgui/mainwindow/mainWindowInterpretersInterface.h
+++ unreal/branches/robots/qrgui/mainwindow/mainWindowInterpretersInterface.h
@@ -10,7 +10,7 @@ class ErrorReporter;
 class MainWindowInterpretersInterface
 {
 public:
-	~MainWindowInterpretersInterface() {};
+	virtual ~MainWindowInterpretersInterface() {}
 	virtual void selectItem(Id const &graphicalId) = 0;
 	virtual void highlight(Id const &graphicalId, bool exclusive = true) = 0;
 	virtual void dehighlight(Id const &graphicalId) = 0;
