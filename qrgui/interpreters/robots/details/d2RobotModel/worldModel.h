05d8b1c63ea7f33e4b0ad6b5903c76aed4f0f839
yurii-litvinov <yurii.litvinov@gmail.com>
2011-06-06 02:12:07 +0400

    stub for WorldModel class.

--- qrgui/interpreters/robots/details/d2RobotModel/worldModel.h
+++ qrgui/interpreters/robots/details/d2RobotModel/worldModel.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <QtCore/QPoint>
+#include <QtCore/QList>
+#include <QtCore/QPair>
+#include <QtGui/QPolygon>
+
+#include "../../sensorConstants.h"
+
 namespace qReal {
 namespace interpreters {
 namespace robots {
@@ -9,6 +16,15 @@ namespace d2Model {
 class WorldModel
 {
 public:
+	int sonarReading(QPoint const &position, qreal direction) const;
+	bool touchSensorReading(QPoint const &position, qreal direction) const;
+	int colorSensorReading(QPoint const &position, sensorType::SensorTypeEnum mode) const;
+	bool checkCollision(QPolygon const &robotRegion) const;
+	QList<QPair<QPoint, QPoint> > const &walls() const;
+	void addWall(QPoint const &begin, QPoint const &end);
+
+private:
+	QList<QPair<QPoint, QPoint> > mWalls;
 };
 
 }
