5434a3bb05855cec381a697d1b2e41aea9c29cc9
yurii-litvinov <yurii.litvinov@gmail.com>
2011-09-24 13:54:02 +0400

    Cleanup

--- plugins/robots/robotsInterpreter/details/robotCommunication/usbRobotCommunicationThread.h
+++ plugins/robots/robotsInterpreter/details/robotCommunication/usbRobotCommunicationThread.h
@@ -21,22 +21,6 @@ public:
 	UsbRobotCommunicationThread();
 	~UsbRobotCommunicationThread();
 
-	// ???
-	QString getBluetoothName();
-	void setBluetoothName(QString name);
-	bool getUseBT();
-	void setUseBT(bool value);
-	unsigned long getBST();
-	void setBST(unsigned long value);
-	QString getPort();
-	void setPort(QString value);
-	QString playTone(unsigned freq, unsigned time);
-
-	QList<QString> NXTListWidget();
-	void runMotor(unsigned motorName, int speed);
-	void stopMotor(unsigned motorName);
-	bool isOpen();
-
 public slots:
 	void send(QObject *addressee, QByteArray const &buffer, unsigned const responseSize);
 	void connect(QString const &portName);
@@ -48,20 +32,12 @@ public slots:
 private:
 	static const int kStatusNoError = 0;
 
-	bool getIsOpen();
+	bool isOpen();
+
 	void send(QByteArray const &buffer, unsigned const responseSize, QObject *addressee);
-	QByteArray receive(int size) const;
 
 	bool mActive;
-	QString mResPort;
-	QString mBTName;
-	bool mUseBT;
-	unsigned long mBluetoothSearchTimeout;
-	QString mPort;
 	unsigned long mNXTHandle;
-	unsigned mAPower;
-	unsigned mBPower;
-
 	robotsInterpreter::robotCommunication::Fantom mFantom;
 };
 
