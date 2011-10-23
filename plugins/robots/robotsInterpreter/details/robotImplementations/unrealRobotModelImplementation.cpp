c9f9baa61b97a5b49ce3364dce9753eacca5c69f
yurii-litvinov <yurii.litvinov@gmail.com>
2011-10-23 21:38:18 +0400

    - fixed warning with painter in sensorItem - some more fixes to initialization

--- plugins/robots/robotsInterpreter/details/robotImplementations/unrealRobotModelImplementation.cpp
+++ plugins/robots/robotsInterpreter/details/robotImplementations/unrealRobotModelImplementation.cpp
@@ -51,12 +51,14 @@ sensorImplementations::UnrealColorSensorImplementation *UnrealRobotModelImplemen
 
 void UnrealRobotModelImplementation::addTouchSensor(inputPort::InputPortEnum const &port)
 {
+	Tracer::debug(tracer::initialization, "UnrealRobotModelImplementation::addTouchSensor", "Configuring touch sensor on port " + QString::number(port));
 	sensorImplementations::UnrealTouchSensorImplementation *sensor = new sensorImplementations::UnrealTouchSensorImplementation(port, mD2Model);
 	mSensorsConfigurer.configureSensor(sensor, port);
 }
 
 void UnrealRobotModelImplementation::addSonarSensor(inputPort::InputPortEnum const &port)
 {
+	Tracer::debug(tracer::initialization, "UnrealRobotModelImplementation::addSonarSensor", "Configuring sonar sensor on port " + QString::number(port));
 	sensorImplementations::UnrealSonarSensorImplementation *sensor = new sensorImplementations::UnrealSonarSensorImplementation(port, mD2Model);
 	mSensorsConfigurer.configureSensor(sensor, port);
 }
@@ -64,6 +66,7 @@ void UnrealRobotModelImplementation::addSonarSensor(inputPort::InputPortEnum con
 void UnrealRobotModelImplementation::addColorSensor(inputPort::InputPortEnum const &port, lowLevelSensorType::SensorTypeEnum mode, sensorType::SensorTypeEnum const &sensorType)
 {
 	Q_UNUSED(mode)
+	Tracer::debug(tracer::initialization, "UnrealRobotModelImplementation::addColorSensor", "Configuring color sensor on port " + QString::number(port));
 	sensorImplementations::UnrealColorSensorImplementation *sensor = new sensorImplementations::UnrealColorSensorImplementation(port, mD2Model, sensorType);
 	mSensorsConfigurer.configureSensor(sensor, port);
 }
