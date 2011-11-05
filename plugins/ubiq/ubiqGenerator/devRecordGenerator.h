fc3d54970f75de471bbf7fe4afac30335757b27f
jzuken <timofey.bryksin@gmail.com>
2011-11-05 17:10:51 +0400

    added loading templates from file

--- plugins/ubiq/ubiqGenerator/devRecordGenerator.h
+++ plugins/ubiq/ubiqGenerator/devRecordGenerator.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "../../../qrgui/toolPluginInterface/usedInterfaces/logicalModelAssistInterface.h"
+#include "../../../qrgui/toolPluginInterface/usedInterfaces/errorReporterInterface.h"
+
+#include <QtCore/QDir>
 
 namespace ubiq {
 namespace generator {
@@ -8,14 +11,27 @@ namespace generator {
 class DevRecordGenerator
 {
 public:
-	DevRecordGenerator(QString const &templateFilePath, qReal::LogicalModelAssistInterface const &logicalModel);
+	DevRecordGenerator(QString const &templateDirPath
+			, QString const &outputDirPath
+			, qReal::LogicalModelAssistInterface const &logicalModel
+			, qReal::ErrorReporterInterface &errorReporter
+			);
 	virtual ~DevRecordGenerator();
 
 	void generate();
 
 private:
+
+	bool changeDir(const QString &path);
+	bool loadTemplateFromFile(QString const &templateFileName, QString &loadedTemplate);
+
+	QString const mTemplateDirPath;
+	QString const mOutputDirPath;
 	qrRepo::LogicalRepoApi const &mApi;
-	QString const mTemplateFilePath;
+	qReal::ErrorReporterInterface &mErrorReporter;
+
+	QDir mDirectory;
+	QString mFileTemplate;
 };
 
 }
