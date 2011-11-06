331877e3077dc7b9be59447241bbaf212be3f92c
jzuken <timofey.bryksin@gmail.com>
2011-11-06 16:44:19 +0400

    generation of switch skeleton

--- plugins/ubiq/ubiqGenerator/dispatcherGenerator.cpp
+++ plugins/ubiq/ubiqGenerator/dispatcherGenerator.cpp
@@ -47,18 +47,39 @@ QString DispatcherGenerator::generateEventHandlers(Id const &diagram)
 {
 	QString eventHadlers;
 	foreach (Id const element, mApi.children(diagram)) { // get elements on master node
-		qDebug() << element.toString();
 		if (!mApi.isLogicalElement(element) || (element.element() != "Handler"))
 			continue;
 
-
-		eventHadlers += generateEventHandler(mApi.name(element));
+		eventHadlers += generateEventHandler(mApi.name(element)); // generate code for each event handler
 	}
 	return eventHadlers;
 }
 
 QString DispatcherGenerator::generateEventHandler(QString const handlerName)
 {
-	return "88";
+	QString handlerCode = mTemplateUtils["@@EventHandler@@"];
+	handlerCode.replace("@@HandlerName@@", handlerName);
+
+	foreach (Id const diagram, mApi.elementsByType("UbiqActivityDiagram")) {
+		// search for the diagram with name handlerName
+		if (!mApi.isLogicalElement(diagram) || (mApi.name(diagram) != handlerName))
+			continue;
+
+		// diagram found, now get HandlerStart-s and generate cases
+
+		QString cases;
+		foreach (Id const &element, mApi.children(diagram)) {
+			if (!mApi.isLogicalElement(diagram) || (element.element() != "HandlerStart"))
+				continue;
+
+			QString caseCode = mTemplateUtils["@@SwitchCase@@"];
+			caseCode.replace("@@CaseValue@@", mApi.name(element));
+
+			cases += caseCode;
+		}
+		handlerCode.replace("@@SwitchCases@@", cases);
+	}
+
+	return handlerCode;
 }
 
