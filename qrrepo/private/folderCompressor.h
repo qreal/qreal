b48f3856789bdeb29763afcaee3760ffcaba1632
Eugeny Novozhilov <eugeny.novozhilov@gmail.com>
2011-12-22 00:16:30 +0400

    styleguide fixes

--- qrrepo/private/folderCompressor.h
+++ qrrepo/private/folderCompressor.h
@@ -7,18 +7,18 @@ class FolderCompressor {
 public:
 	FolderCompressor();
 
-	///A recursive function that scans all files inside the source folder
-	///and serializes all files in a row of file names and compressed
-	///binary data in a single file
+	/// A recursive function that scans all files inside the source folder
+	/// and serializes all files in a row of file names and compressed
+	/// binary data in a single file
 	bool compressFolder(QString const &sourceFolder, QString const&destinationFile);
 
-	///A function that deserializes data from the compressed file and
-	///creates any needed subfolders before saving the file
+	/// A function that deserializes data from the compressed file and
+	/// creates any needed subfolders before saving the file
 	bool decompressFolder(QString const &sourceFile, QString const &destinationFolder);
 private:
 	QFile mFile;
 	QDataStream mDataStream;
-	bool compress(QString const &sourceFolder, QString const &prefex);
+	bool compress(QString const &sourceFolder, QString const &prefix);
 };
 
 
