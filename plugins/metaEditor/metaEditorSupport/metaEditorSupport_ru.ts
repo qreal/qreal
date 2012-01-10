b8ba271e4676ff8a2422f985180f7e5e1e139103
yurii-litvinov <yurii.litvinov@gmail.com>
2012-01-10 23:01:58 +0400

    added missing .ts file

---
+++ plugins/metaEditor/metaEditorSupport/metaEditorSupport_ru.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="editorGenerator.cpp" line="110"/>
+        <source>incorrect file name</source>
+        <translation>Неверное имя файла</translation>
+    </message>
+</context>
+<context>
+    <name>metaEditor::MetaEditorSupportPlugin</name>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="39"/>
+        <source>Generate editor</source>
+        <translation>Генерировать редактор</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="43"/>
+        <source>Generate editor (qrmc)</source>
+        <translation>Генерировать редактор (QRMC)</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="47"/>
+        <source>Parse editor xml</source>
+        <translation>Импортировать xml с редактором</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="72"/>
+        <source>loading..</source>
+        <translation>Загрузка..</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="72"/>
+        <source>Do you want to load generated editor %1?</source>
+        <translation>Вы хотите загрузить сгенерированный редактор %1?</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="85"/>
+        <source>There is nothing to generate</source>
+        <translation>Метамоделей для генерации не найдено</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="108"/>
+        <location filename="metaEditorSupportPlugin.cpp" line="127"/>
+        <location filename="metaEditorSupportPlugin.cpp" line="152"/>
+        <source>error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="108"/>
+        <source>please, fill compiler settings</source>
+        <translation>Пожалуйста, заполните настройки компилятора в окне &quot;Настройки&quot;</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="127"/>
+        <source>cannot unload plugin</source>
+        <translation>Не удалось выгрузить старый плагин</translation>
+    </message>
+    <message>
+        <location filename="metaEditorSupportPlugin.cpp" line="152"/>
+        <source>cannot load new editor</source>
+        <translation>Не удалось загрузить нвый редактор</translation>
+    </message>
+</context>
+</TS>
