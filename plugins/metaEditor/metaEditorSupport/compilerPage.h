8788f9f191e738a415533bdaa4fc09d41f62aa65
yurii-litvinov <yurii.litvinov@gmail.com>
2012-01-26 23:46:17 +0400

    Some more comments

--- plugins/metaEditor/metaEditorSupport/compilerPage.h
+++ plugins/metaEditor/metaEditorSupport/compilerPage.h
@@ -8,12 +8,16 @@ namespace Ui {
 
 namespace metaEditor {
 
+/// Page with plugin compiler settings to be shown in preferences dialog
 class PreferencesCompilerPage : public PreferencesPage
 {
 	Q_OBJECT
 
 public:
+	/// Constructor
+	/// @param parent Parent widget
 	explicit PreferencesCompilerPage(QWidget *parent = 0);
+
 	~PreferencesCompilerPage();
 
 	void save();
@@ -22,13 +26,16 @@ protected:
 	void changeEvent(QEvent *e);
 
 private slots:
+	/// Slot for switching between OS presets
 	void changeSystem();
 
 private:
+	/// Helper function to init form fields after preset selection
 	void initCompilersSettings(QString const &pathToQmake,
 			QString const &pathToMake, QString const &pluginExtension, QString const &prefix);
 
-	Ui::PreferencesCompilerPage *mUi;
+	/// Ui object
+	Ui::PreferencesCompilerPage *mUi;  // Has ownership
 };
 
 }
