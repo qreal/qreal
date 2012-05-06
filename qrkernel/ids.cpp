a95e5c31116392920ead6c6acb4c569217764135
yurii-litvinov <yurii.litvinov@gmail.com>
2012-05-06 22:38:08 +0400

    styleguided Ids.

--- qrkernel/ids.cpp
+++ qrkernel/ids.cpp
@@ -7,20 +7,20 @@ using namespace qReal;
 
 Id Id::loadFromString(QString const &string)
 {
-	QStringList path = string.split('/');
+	QStringList const path = string.split('/');
 	Q_ASSERT(path.count() > 0 && path.count() <= 5);
 	Q_ASSERT(path[0] == "qrm:");
 
 	Id result;
 	switch (path.count()) {
-		case 5: result.mId = path[4];
-			// Fall-thru
-		case 4: result.mElement = path[3];
-			// Fall-thru
-		case 3: result.mDiagram = path[2];
-			// Fall-thru
-		case 2: result.mEditor = path[1];
-			// Fall-thru
+	case 5: result.mId = path[4];
+		// Fall-thru
+	case 4: result.mElement = path[3];
+		// Fall-thru
+	case 3: result.mDiagram = path[2];
+		// Fall-thru
+	case 2: result.mEditor = path[1];
+		// Fall-thru
 	}
 	Q_ASSERT(string == result.toString());
 	Q_ASSERT(string == result.toUrl().toString());
@@ -38,30 +38,36 @@ Id Id::rootId()
 }
 
 Id::Id(QString const &editor, QString  const &diagram, QString  const &element, QString  const &id)
-	: mEditor(editor), mDiagram(diagram), mElement(element), mId(id)
+	: mEditor(editor)
+	, mDiagram(diagram)
+	, mElement(element)
+	, mId(id)
 {
 	Q_ASSERT(checkIntegrity());
 }
 
 Id::Id(Id const &base, QString const &additional)
-	: mEditor(base.mEditor), mDiagram(base.mDiagram), mElement(base.mElement), mId(base.mId)
+	: mEditor(base.mEditor)
+	, mDiagram(base.mDiagram)
+	, mElement(base.mElement)
+	, mId(base.mId)
 {
-	unsigned baseSize = base.idSize();
+	unsigned const baseSize = base.idSize();
 	switch (baseSize) {
-		case 0:
-			mEditor = additional;
-			break;
-		case 1:
-			mDiagram = additional;
-			break;
-		case 2:
-			mElement = additional;
-			break;
-		case 3:
-			mId = additional;
-			break;
-		default:
-			Q_ASSERT(!"Can not add a part to Id, it will be too long");
+	case 0:
+		mEditor = additional;
+		break;
+	case 1:
+		mDiagram = additional;
+		break;
+	case 2:
+		mElement = additional;
+		break;
+	case 3:
+		mId = additional;
+		break;
+	default:
+		Q_ASSERT(!"Can not add a part to Id, it will be too long");
 	}
 	Q_ASSERT(checkIntegrity());
 }
@@ -98,14 +104,15 @@ Id Id::sameTypeId() const
 
 unsigned Id::idSize() const
 {
-	if (mId != "")
+	if (!mId.isEmpty()) {
 		return 4;
-	if (mElement != "")
+	} if (!mElement.isEmpty()) {
 		return 3;
-	if (mDiagram != "")
+	} if (!mDiagram.isEmpty()) {
 		return 2;
-	if (mEditor != "")
+	} if (!mEditor.isEmpty()) {
 		return 1;
+	}
 	return 0;
 }
 
@@ -117,12 +124,13 @@ QUrl Id::toUrl() const
 QString Id::toString() const
 {
 	QString path = "qrm:/" + mEditor;
-	if (mDiagram != "")
+	if (mDiagram != "") {
 		path += "/" + mDiagram;
-	if (mElement != "")
+	} if (mElement != "") {
 		path += "/" + mElement;
-	if (mId != "")
+	} if (mId != "") {
 		path += "/" + mId;
+	}
 	return path;
 }
 
@@ -130,21 +138,25 @@ bool Id::checkIntegrity() const
 {
 	bool emptyPartsAllowed = true;
 
-	if (mId != "")
+	if (!mId.isEmpty()) {
 		emptyPartsAllowed = false;
+	}
 
-	if (mElement != "")
+	if (!mElement.isEmpty()) {
 		emptyPartsAllowed = false;
-	else if (!emptyPartsAllowed)
+	} else if (!emptyPartsAllowed) {
 		return false;
+	}
 
-	if (mDiagram != "")
+	if (!mDiagram.isEmpty()) {
 		emptyPartsAllowed = false;
-	else if (!emptyPartsAllowed)
+	} else if (!emptyPartsAllowed) {
 		return false;
+	}
 
-	if (mEditor == "" && !emptyPartsAllowed)
+	if (mEditor.isEmpty() && !emptyPartsAllowed) {
 		return false;
+	}
 
 	return true;
 }
