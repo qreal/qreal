afce5937849093931a3253a10866ef257149264e
osechkina_masha <osechkina.masha@gmail.com>
2012-06-28 19:07:44 +0400

    add pattern for DataIntegrator

---
+++ plugins/sbt/sbtGenerator/template/DataIntegrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class @@DataIntegrator@@
+{
+	public @@DataIntegrator@@()
+	{
+	}
+    private string query = "@@queryString@@";
+    public @@ReturnDataObject@@ executeQuery(@@ParameterDataObject@@ dataObject)
+    {
+        return new @@ReturnDataObject@@();
+    }
+}
