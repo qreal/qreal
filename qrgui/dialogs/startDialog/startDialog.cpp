9d5765c9cbabc31016baf7a1c0ac06b82d27cb68
ramntry <ramntry@gmail.com>
2012-07-06 14:07:51 +0400

    command links added into StartDialog

--- qrgui/dialogs/startDialog/startDialog.cpp
+++ qrgui/dialogs/startDialog/startDialog.cpp
@@ -1,4 +1,5 @@
 #include <QtGui/QTabWidget>
+#include <QtGui/QCommandLinkButton>
 #include "startDialog.h"
 #include "suggestToCreateDiagramWidget.h"
 #include "recentProjectsListWidget.h"
@@ -7,10 +8,31 @@
 qReal::StartDialog::StartDialog(MainWindow *mainWindow, QWidget *parent)
 	: QDialog(parent)
 	, mMainWindow(mainWindow)
-	, mTabWidget(new QTabWidget(this))
+	, mTabWidget(new QTabWidget)
 {
-	setFixedSize(512, 384);
+	setFixedSize(700, 384);
 	mTabWidget->addTab(new RecentProjectsListWidget(this), tr("Recent projects"));
 	mTabWidget->addTab(new SuggestToCreateDiagramWidget(mMainWindow, this), tr("New project with diagram"));
 	mTabWidget->setFixedSize(480, 300);
+
+	QCommandLinkButton *createNewProjectLink = new QCommandLinkButton(tr("&Create new project"));
+	QCommandLinkButton *helpLink = new QCommandLinkButton(tr("&Help"));
+	QCommandLinkButton *quitLink = new QCommandLinkButton(tr("&Quit QReal"));
+
+	QVBoxLayout *commandLinksLayout = new QVBoxLayout;
+	commandLinksLayout->addWidget(createNewProjectLink);
+	commandLinksLayout->addWidget(helpLink);
+	commandLinksLayout->addWidget(quitLink);
+	commandLinksLayout->insertStretch(0);
+	commandLinksLayout->insertStretch(-1);
+
+	QHBoxLayout *mainLayout = new QHBoxLayout;
+	mainLayout->addWidget(mTabWidget);
+	mainLayout->addLayout(commandLinksLayout);
+
+	setLayout(mainLayout);
+
+	connect(createNewProjectLink, SIGNAL(clicked()), mMainWindow, SLOT(createProject()));
+	connect(helpLink, SIGNAL(clicked()), mMainWindow, SLOT(showHelp()));
+	connect(quitLink, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
