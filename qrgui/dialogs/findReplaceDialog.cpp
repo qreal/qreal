a89b3ce9f9c27fefad22809e08fdc44e38b9db7a
parradox <rauf.kurbanov@student.spbu.ru>
2012-07-10 16:55:08 +0400

    aligned with the style guide

--- qrgui/dialogs/findReplaceDialog.cpp
+++ qrgui/dialogs/findReplaceDialog.cpp
@@ -13,7 +13,7 @@ FindReplaceDialog::FindReplaceDialog(qrRepo::LogicalRepoApi const &logicalRepoAp
 	mCheckBoxes.append(mUi->mByPropertyBox);
 	mCheckBoxes.append(mUi->mByContentBox);
 	mCheckBoxes.append(mUi->mSensitivity);
-    mCheckBoxes.append(mUi->mByRegExp);
+	mCheckBoxes.append(mUi->mByRegExp);
 
 	mCheckBoxes.first()->setChecked(true);
 
@@ -31,8 +31,8 @@ FindReplaceDialog::FindReplaceDialog(qrRepo::LogicalRepoApi const &logicalRepoAp
 
 void FindReplaceDialog::tryEnableReplaceButton()
 {
-    mUi->mReplaceButton->setEnabled((mUi->mByNameBox->isChecked() || mUi->mByContentBox->isChecked())
-            && (!mUi->mByPropertyBox->isChecked()) && (!mUi->mByTypeBox->isChecked()));
+	mUi->mReplaceButton->setEnabled((mUi->mByNameBox->isChecked() || mUi->mByContentBox->isChecked())
+			&& (!mUi->mByPropertyBox->isChecked()) && (!mUi->mByTypeBox->isChecked()));
 }
 
 void FindReplaceDialog::stateClear()
@@ -108,5 +108,6 @@ void FindReplaceDialog::initIds(QMap<QString, QString> foundData)
 		}
 	}
 
-	QObject::connect(mUi->mListWidget, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(itemChosen(QListWidgetItem*)));
+	QObject::connect(mUi->mListWidget, SIGNAL(itemClicked(QListWidgetItem*)), this,
+			SLOT(itemChosen(QListWidgetItem*)));
 }
--- qrgui/mainwindow/findManager.cpp
+++ qrgui/mainwindow/findManager.cpp
@@ -22,14 +22,14 @@ qReal::IdList FindManager::foundByMode(QString key, QString currentMode, bool se
 {
 	// TODO: replace mode string with modifiers
 	if (currentMode == tr("by name")) {
-        return mControlApi.findElementsByName(key, sensitivity, regExpression);
+		return mControlApi.findElementsByName(key, sensitivity, regExpression);
 	} else if (currentMode == tr("by type")) {
-        return mLogicalApi.elementsByType(key, sensitivity, regExpression);
+		return mLogicalApi.elementsByType(key, sensitivity, regExpression);
 	} else if (currentMode == tr("by property")) {
-        return mControlApi.elementsByProperty(key, sensitivity, regExpression);
+		return mControlApi.elementsByProperty(key, sensitivity, regExpression);
 	} else if (currentMode == tr("by property content")) {
-        return mControlApi.elementsByPropertyContent(key, sensitivity, regExpression);
-    } else {
+		return mControlApi.elementsByPropertyContent(key, sensitivity, regExpression);
+	} else {
 		return qReal::IdList();
 	}
 }
@@ -37,22 +37,22 @@ qReal::IdList FindManager::foundByMode(QString key, QString currentMode, bool se
 QMap<QString, QString> FindManager::findItems(QStringList const &searchData)
 {
 	QMap<QString, QString> found;
-    bool sensitivity = searchData.contains(tr("case sensitivity"));
-    bool regExpression = searchData.contains(tr("by regular expression"));
+	bool sensitivity = searchData.contains(tr("case sensitivity"));
+	bool regExpression = searchData.contains(tr("by regular expression"));
 
 
-    for(int i = 1; i < searchData.length(); i++) {
-        if (searchData[i] != tr("case sensitivity") && searchData[i] != tr("by regular expression")) {
-            qReal::IdList byMode = foundByMode(searchData.first(), searchData[i], sensitivity, regExpression);
+	for(int i = 1; i < searchData.length(); i++) {
+		if (searchData[i] != tr("case sensitivity") && searchData[i] != tr("by regular expression")) {
+			qReal::IdList byMode = foundByMode(searchData.first(), searchData[i], sensitivity, regExpression);
 			foreach (qReal::Id currentId, byMode) {
 				if (found.contains(currentId.toString())) {
 					found[currentId.toString()] += tr(", ") + searchData[i];
 					continue;
 				}
 				found.insert(currentId.toString(), tr("   :: ") + searchData[i]);
-            }
-        }
-    }
+			}
+		}
+	}
 	return found;
 }
 
@@ -64,17 +64,17 @@ void FindManager::handleFindDialog(QStringList const &searchData)
 void FindManager::handleReplaceDialog(QStringList &searchData)
 {
 	if (searchData.contains(tr("by name"))) {
-        qReal::IdList toRename = foundByMode(searchData.first(), tr("by name")
-                                             , searchData.contains(tr("case sensitivity")),
-                                             searchData.contains(tr("by regular expression")));
+		qReal::IdList toRename = foundByMode(searchData.first(), tr("by name")
+											 , searchData.contains(tr("case sensitivity")),
+											 searchData.contains(tr("by regular expression")));
 		foreach (qReal::Id currentId, toRename) {
 			mLogicalApi.setName(currentId, searchData[1]);
 		}
 	}
 	if (searchData.contains(tr("by property content"))) {
-        qReal::IdList toRename = foundByMode(searchData.first(), tr("by property content")
-                        , searchData.contains(tr("case sensitivity")),
-                                             searchData.contains(tr("by regular expression")));
+		qReal::IdList toRename = foundByMode(searchData.first(), tr("by property content")
+						, searchData.contains(tr("case sensitivity")),
+											 searchData.contains(tr("by regular expression")));
 		mLogicalApi.replaceProperties(toRename, searchData[0], searchData[1]);
 	}
 }
