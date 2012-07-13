77daacc83bbe3748a2f1e1dd6543d7cdd5469121
ramntry <ramntry@gmail.com>
2012-07-13 15:12:53 +0400

    some changes in doxygen docs according to styleguide

--- qrgui/dialogs/startDialog/listWidget.h
+++ qrgui/dialogs/startDialog/listWidget.h
@@ -19,9 +19,9 @@ class ListWidget : public QWidget
 public:
 	explicit ListWidget(QWidget *parent = 0);
 
-	/// @param text - item label
-	/// @param userData - associated with item the string data
-	/// @param toolTip - item tooltip
+	/// @param text Item label
+	/// @param userData Associated with item the string data
+	/// @param toolTip Item tooltip
 	void addItem(QString const &text, QString const &userData, QString const &toolTip = "");
 	
 signals:
