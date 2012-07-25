06c979fefb580d63ec1c40776de43c856f12a4d3
MariaTikhonova <maria_tikhonova@list.ru>
2012-07-25 16:24:27 +0400

    added commentaries

--- plugins/exterminatus/exterminatusPlugin.h
+++ plugins/exterminatus/exterminatusPlugin.h
@@ -4,6 +4,8 @@
 
 namespace exterminatus {
 
+/// Plugin to exterminate biosphere of the planet
+/// and all elements from repository.
 class ExterminatusPlugin : public QObject, public qReal::ToolPluginInterface
 {
 	Q_OBJECT
@@ -18,11 +20,14 @@ public:
 	virtual void init(qReal::PluginConfigurator const &configurator);
 
 private slots:
+	/// deletion of all items from repository
 	void exterminate();
 
 private:
 	qrRepo::RepoControlInterface *mRepo;  // Doesn't have ownership
 	QAction mAction;
+
+	/// To syncronize logical and graphical models with repository.
 	qReal::gui::MainWindowInterpretersInterface *mMainWindowInterpretersInterface;
 
 };
