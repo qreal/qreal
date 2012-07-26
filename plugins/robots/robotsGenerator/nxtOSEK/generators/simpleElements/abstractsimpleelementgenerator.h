540bbcc6e89bb7fd3e9e50bac72a08d3c195e24d
SashaPishnova <crysids@mail.ru>
2012-07-26 23:12:59 +0400

    generation of sensors variables/AGAIN

--- plugins/robots/robotsGenerator/nxtOSEK/generators/simpleElements/abstractsimpleelementgenerator.h
+++ plugins/robots/robotsGenerator/nxtOSEK/generators/simpleElements/abstractsimpleelementgenerator.h
@@ -9,17 +9,17 @@ class NxtOSEKRobotGenerator;
 
 class AbstractSimpleElementGenerator {
 public:
-    AbstractSimpleElementGenerator();
-    virtual ~AbstractSimpleElementGenerator();
+	AbstractSimpleElementGenerator();
+	virtual ~AbstractSimpleElementGenerator();
 
-    static QList<SmartLine> convertedCode(NxtOSEKRobotGenerator *nxtGen
-            , qReal::Id const elementId, qReal::Id const logicElementId);
+	static QList<SmartLine> convertedCode(NxtOSEKRobotGenerator *nxtGen
+			, qReal::Id const elementId, qReal::Id const logicElementId);
 
 protected:
-    virtual QList<SmartLine> convertElementIntoDirectCommand(NxtOSEKRobotGenerator *nxtGen
-           , qReal::Id const elementId, qReal::Id const logicElementId) = 0;
-    virtual QList<QString> portsToEngineNames(QString const &portsProperty) const;
-    virtual QString transformSign(QString const &sign) const;
+	virtual QList<SmartLine> convertElementIntoDirectCommand(NxtOSEKRobotGenerator *nxtGen
+		   , qReal::Id const elementId, qReal::Id const logicElementId) = 0;
+	virtual QList<QString> portsToEngineNames(QString const &portsProperty);
+	virtual QString transformSign(QString const &sign);
 };
 }
 }
