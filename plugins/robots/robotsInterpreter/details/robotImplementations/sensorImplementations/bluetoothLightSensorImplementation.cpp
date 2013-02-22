839f5bbf8e8f7e83e4022f4c9eca6df6591eecb9
Dmitry Mordvinov <dvvsrd@gmail.com>
2013-02-22 15:28:19 +0400

    An attempt to implement light sensor

--- plugins/robots/robotsInterpreter/details/robotImplementations/sensorImplementations/bluetoothLightSensorImplementation.cpp
+++ plugins/robots/robotsInterpreter/details/robotImplementations/sensorImplementations/bluetoothLightSensorImplementation.cpp
@@ -14,5 +14,34 @@ BluetoothLightSensorImplementation::BluetoothLightSensorImplementation(RobotComm
 
 void BluetoothLightSensorImplementation::read()
 {
+	if (!mIsConfigured) {
+		// If sensor is not configured, report failure and return immediately.
+		// It is not an error, it shall be possible to reconfigure sensor "on the fly",
+		// but when it is reconfiguring it shall not be available.
+		emit failure();
+		return;
+	}
 
+	if (mState == pending) {
+		return;
+	}
+
+	mState = pending;
+	QByteArray command(5, 0);
+	command[0] = 0x03;  //command length
+	command[1] = 0x00;
+	command[2] = telegramType::directCommandResponseRequired;
+	command[3] = commandCode::GETINPUTVALUES;
+	command[4] = mPort;
+	mRobotCommunicationInterface->send(this, command, 18);
+}
+
+#include<QDebug>
+void BluetoothLightSensorImplementation::sensorSpecificProcessResponse(QByteArray const &reading)
+{
+	mState = idle;
+	int sensorValue = (0xff & reading[13]) << 8 | (0xff & reading[14]);
+	Tracer::debug(tracer::sensors, "BluetoothLightSensorImplementation::sensorSpecificProcessResponse", QString::number(sensorValue));
+	qDebug() << sensorValue;
+	emit response(sensorValue);
 }
