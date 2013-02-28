77077f4b0a7d13fde4346a61a53955fd058e0bff
dima6120 <dima6120@gmail.com>
2013-02-28 19:53:33 +0300

    little style changes

--- qrutils/mathUtils/gaussNoise.h
+++ qrutils/mathUtils/gaussNoise.h
@@ -2,31 +2,34 @@
 
 #include <QtCore/QtGlobal>
 
+#include "../utilsDeclSpec.h"
+
 namespace mathUtils
 {
 
 qreal const mu = 0.5;
 qreal const var = 0.083; // 1/12
 
-class GaussNoise
+/// Provides Gauss Noise Generator
+class QRUTILS_EXPORT GaussNoise
 {
 public:
     GaussNoise();
     GaussNoise(unsigned int const approximationLevel, qreal const variance);
     ~GaussNoise();
 
-    // Gauss noise generator. Uses Central Limit Thorem Method
+    /// Gauss noise generator. Uses Central Limit Thorem Method
     qreal generate(unsigned int const approximationLevel, qreal const variance);
     qreal generate();
 
-    // Functions for working with properties
+    /// Functions for working with properties
     void setApproximationLevel(unsigned int const approximationLevel);
     void setDispersion(qreal const variance);
 
     GaussNoise operator >> (qreal &left);
 
 private:
-    // Body of function 'generate'. Uses for various realizations of 'generate'
+    /// Body of function 'generate'. Uses for various realizations of 'generate'
     qreal genBody(unsigned int const approximationLevel, qreal const variance);
 
     unsigned int mApproximationLevel;
