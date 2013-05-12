30600718b492724909379cdc0c09da799d41b7f7
dima6120 <dima6120@gmail.com>
2013-05-13 00:44:01 +0400

    added hotkey action collector for plugins

---
+++ qrgui/toolPluginInterface/hotKeyActionInfo.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <QtCore/QString>
+#include <QtWidgets/QAction>
+
+namespace qReal {
+
+class HotKeyActionInfo {
+public:
+	HotKeyActionInfo(QString const id, QString const label, QAction *action)
+		: mAction(action)
+		, mId(id)
+		, mLabel(label)
+	{
+
+	}
+
+	QAction *action() const
+	{
+		return mAction;
+	}
+
+	QString id() const
+	{
+		return mId;
+	}
+
+	QString label() const
+	{
+		return mLabel;
+	}
+
+private:
+	QAction *mAction;
+	QString mId;
+	QString mLabel;
+};
+
+}
+
--- qrgui/toolPluginInterface/toolPluginInterface.pri
+++ qrgui/toolPluginInterface/toolPluginInterface.pri
@@ -8,3 +8,4 @@ HEADERS += \
 	toolPluginInterface/usedInterfaces/details/modelsAssistInterface.h \
 	toolPluginInterface/usedInterfaces/graphicalModelAssistInterface.h \
 	toolPluginInterface/usedInterfaces/logicalModelAssistInterface.h \
+    toolPluginInterface/hotKeyActionInfo.h
