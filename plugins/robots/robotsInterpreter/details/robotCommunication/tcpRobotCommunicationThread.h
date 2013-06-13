097a84ca73a2dc04942dbbb34ca48e525de22d75
Yurii Litvinov <yurii.litvinov@gmail.com>
2013-06-13 14:05:56 +0400

    added missing filed

---
+++ plugins/robots/robotsInterpreter/details/robotCommunication/tcpRobotCommunicationThread.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <QtNetwork/QTcpSocket>
+
+#include "robotCommunicationThreadInterface.h"
+
+namespace qReal
+{
+namespace interpreters
+{
+namespace robots
+{
+namespace details
+{
+
+class TcpRobotCommunicationThread : public RobotCommunicationThreadInterface
+{
+	Q_OBJECT
+
+public:
+	TcpRobotCommunicationThread();
+	virtual ~TcpRobotCommunicationThread();
+
+public slots:
+	virtual void send(QObject *addressee, QByteArray const &buffer, unsigned const responseSize);
+	virtual void sendI2C(QObject *addressee, QByteArray const &buffer
+			, unsigned const responseSize, inputPort::InputPortEnum const &port);
+	virtual void connect();
+	virtual void disconnect();
+	virtual void reconnect();
+	virtual void allowLongJobs(bool allow = true);
+	virtual void checkConsistency();
+
+private:
+	QTcpSocket *mSocket;
+};
+
+}
+}
+}
+}
