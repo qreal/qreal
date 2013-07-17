c2c9d43c1b2a657e82dbe88eb5872429b7030155
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-07-17 15:21:53 +0400

    Path to copy added

--- buildScripts/vera++/CopyToVLib.sh
+++ buildScripts/vera++/CopyToVLib.sh
@@ -1,3 +1,4 @@
 sudo cp buildScripts/vera++/profiles/* /usr/lib/vera++/profiles/
 sudo cp buildScripts/vera++/rules/* /usr/lib/vera++/scripts/rules/
-
+sudo cp buildScripts/vera++/profiles/* /usr/local/lib/vera++/profiles/
+sudo cp buildScripts/vera++/rules/* /usr/local/lib/vera++/scripts/rules/
