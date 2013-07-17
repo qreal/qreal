279613bb595c00f214349f0bb9abc4caa840d368
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-07-17 15:49:12 +0400

    Redundant actions removed

--- buildScripts/vera++/CopyToVLib.sh
+++ buildScripts/vera++/CopyToVLib.sh
@@ -1,4 +1,4 @@
-sudo cp buildScripts/vera++/profiles/* /usr/lib/vera++/profiles/
-sudo cp buildScripts/vera++/rules/* /usr/lib/vera++/scripts/rules/
+#! /bin/bash
+
 sudo cp buildScripts/vera++/profiles/* /usr/local/lib/vera++/profiles/
 sudo cp buildScripts/vera++/rules/* /usr/local/lib/vera++/scripts/rules/
