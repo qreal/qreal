018e9e67edb91e6de1faa41eefa256d14388c740
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-07-17 23:00:00 +0400

    Paths fixed

--- buildScripts/vera++/CopyToVLib.sh
+++ buildScripts/vera++/CopyToVLib.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo cp buildScripts/vera++/profiles/* /usr/local/lib/vera++/profiles/
-sudo cp buildScripts/vera++/rules/* /usr/local/lib/vera++/scripts/rules/
+sudo cp buildScripts/vera++/profiles/* /usr/lib/vera++/profiles/
+sudo cp buildScripts/vera++/rules/* /usr/lib/vera++/scripts/rules/
