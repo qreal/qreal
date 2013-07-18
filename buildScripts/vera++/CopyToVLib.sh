7aa40075379ee878f989bc8868e0431e750eeaeb
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-07-18 15:40:44 +0400

    Build instructions fixed

--- buildScripts/vera++/CopyToVLib.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo cp buildScripts/vera++/profiles/* /usr/lib/vera++/profiles/
-sudo cp buildScripts/vera++/rules/* /usr/lib/vera++/scripts/rules/
--- buildScripts/vera++/rules/IncludeOrder.tcl
+++ buildScripts/vera++/rules/IncludeOrder.tcl
@@ -10,18 +10,33 @@ proc checkIncludeOrder { fileName } {
       incr parentH
       if {$parentH == 1} { 
       incr lineCount
-      continue }
+      set nextLine [getLine $fileName [expr $lineCount + 1]]
+      if {![regexp {^$} $nextLine]} {
+	report $fileName $[expr $lineCount + 1] "No new line after include"
+      }
+      continue 
+      }
      } 
      if [regexp {[\t ]*#include[\t ]*<[^>]*>[ \t]*$} $line] {
 	if {$parentH == 0} { report $fileName $lineCount "Invalid order of includes" }
-	if {$systemConnected == 1} {report $fileName $lineCount "Invalid order of includes" }
+	if {$systemConnected == 1} {report $fileName $lineCount "Invalid order of include" }
 	incr lineCount
 	continue
      }
      if [regexp {[\t ]*#include[\t ]*\"[^\"]*\"[ \t]*$} $line] {
+	if {!$systemConnected} {
+	  set nextLine [getLine $fileName [expr $lineCount + 1]]
+	  if {![regexp {^$} $nextLine]} {
+	    eport $fileName $[expr $lineCount + 1] "No new line after include"
+	  }
+	}
 	set systemConnected 1
 	if {$parentH == 0} { report $fileName $lineCount "Invalid order of includes" }
+	continue
      }
+     if {![regexp {^$} $line]} {
+      set prevLine [getLine $fileName [expr $lineCount - 1]]
+      if {![regexp {^$} $prevLine]} { report $fileName $lineCount "No new line after include"
      incr lineCount
   }
 }
