bf51d2b7e0f820aec74cd786d7e2d3647f0bafac
jzuken <timofey.bryksin@gmail.com>
2013-07-19 17:50:51 +0400

    fixed version

--- plugins/bpmn/help/version.js
+++ plugins/bpmn/help/version.js
@@ -1,8 +1,8 @@
-var version = "0.1";
+var version = "0.2.0";
 var year = 2013;
 
 window.onload = function() {
-	document.title = document.title.replace("%version%", version);
-	document.body.innerHTML = document.body.innerHTML.replace("%version%", version);
-	document.body.innerHTML = document.body.innerHTML.replace("%year%", year);
+    document.title = document.title.replace("%version%", version);
+    document.body.innerHTML = document.body.innerHTML.replace("%version%", version);
+    document.body.innerHTML = document.body.innerHTML.replace("%year%", year);
 }
