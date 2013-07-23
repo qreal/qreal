544b69578b020b0de82d78c4e53f38fafc039da4
MariaTikhonova <maria_tikhonova@list.ru>
2013-07-23 16:25:39 +0400

    added translation files

---
+++ qrtest/editorPluginTestingFramework/editorPluginTestingFramework_ru.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="43"/>
+        <source>no directory to generated code or path to QReal Source Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="89"/>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="620"/>
+        <source>wrong name
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="113"/>
+        <source>incorrect file name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="270"/>
+        <source>not filled name/importedFrom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="341"/>
+        <source>Incorrect label type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugins/metaEditor/metaEditorSupport/editorGenerator.cpp" line="612"/>
+        <source>not filled %1
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Results are the same</source>
+        <translation type="obsolete">Результаты тестирования методов совпадают</translation>
+    </message>
+    <message>
+        <source>THIS METHOD HAS TO BE VERIFIED SOMEHOW!</source>
+        <translation type="obsolete">Метод не тестируется, его нужно проверить!</translation>
+    </message>
+    <message>
+        <source>Results are not the same</source>
+        <translation type="obsolete">Результаты не совпадают</translation>
+    </message>
+    <message>
+        <location filename="htmlMaker.cpp" line="33"/>
+        <source>Table with results of comparison between qrxc and qrmc</source>
+        <translation>Таблица с результатами сравнения qrxc и qrmc</translation>
+    </message>
+    <message>
+        <location filename="htmlMaker.cpp" line="34"/>
+        <location filename="htmlMaker.cpp" line="37"/>
+        <source>Method name</source>
+        <translation>Имя метода</translation>
+    </message>
+    <message>
+        <location filename="htmlMaker.cpp" line="36"/>
+        <source>Table with results of comparison between qrxc and interpreter</source>
+        <translation>Таблица с результатами сравнения qrxc и интерпретатора</translation>
+    </message>
+</context>
+<context>
+    <name>qReal::EditorManager</name>
+    <message>
+        <location filename="../../qrgui/pluginManager/editorManager.cpp" line="117"/>
+        <location filename="../../qrgui/pluginManager/editorManager.cpp" line="132"/>
+        <source>error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/pluginManager/editorManager.cpp" line="117"/>
+        <source>Plugin loading failed: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/pluginManager/editorManager.cpp" line="132"/>
+        <source>Plugin unloading failed: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>qReal::gui::ErrorReporter</name>
+    <message>
+        <location filename="../../qrgui/mainwindow/errorReporter.cpp" line="152"/>
+        <source>INFORMATION:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/mainwindow/errorReporter.cpp" line="154"/>
+        <source>WARNING:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/mainwindow/errorReporter.cpp" line="156"/>
+        <source>ERROR:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/mainwindow/errorReporter.cpp" line="158"/>
+        <source>CRITICAL:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
