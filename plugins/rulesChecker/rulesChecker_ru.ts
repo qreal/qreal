b14ec52c44d8b4644613ffa05e17fd9e2a62a772
Ashatta <tatjana.agapova@gmail.com>
2013-07-25 12:18:44 +0400

    documentation: expanding subprocesses and new types of flows; translations updated

--- plugins/rulesChecker/rulesChecker_ru.ts
+++ plugins/rulesChecker/rulesChecker_ru.ts
@@ -29,6 +29,16 @@
         <translation>Вернуться в более общий процесс</translation>
     </message>
     <message>
+        <location filename="bpmnCustomizer.cpp" line="55"/>
+        <source>Expand subprocess</source>
+        <translation>Развернуть подпроцесс</translation>
+    </message>
+    <message>
+        <location filename="bpmnCustomizer.cpp" line="60"/>
+        <source>Collapse subprocess</source>
+        <translation>Свернуть подпроцесс</translation>
+    </message>
+    <message>
         <location filename="rulesBPMNPlugin.cpp" line="38"/>
         <source>Diagram Validation</source>
         <translation>Проверить диаграммы</translation>
@@ -42,42 +52,42 @@
 <context>
     <name>qReal::rulesChecker::RulesChecker</name>
     <message>
-        <location filename="rulesChecker.cpp" line="127"/>
+        <location filename="rulesChecker.cpp" line="128"/>
         <source>All diagrams compiled without errors</source>
         <translation>Все диаграммы корректны</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="140"/>
+        <location filename="rulesChecker.cpp" line="141"/>
         <source>Current diagram compiled without errors</source>
         <translation>Текущая диаграмма корректна</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="150"/>
+        <location filename="rulesChecker.cpp" line="151"/>
         <source>There are links to start node</source>
         <translation>Входящий поток управления к начальному событию</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="154"/>
+        <location filename="rulesChecker.cpp" line="155"/>
         <source>There are links from End-event</source>
         <translation>Исходящий поток управления из завершающего события</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="158"/>
+        <location filename="rulesChecker.cpp" line="159"/>
         <source>There is no start-node in path</source>
         <translation>В потоке управления нет начального события</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="162"/>
+        <location filename="rulesChecker.cpp" line="163"/>
         <source>There is no end-node in path</source>
         <translation>В потоке управления нет завершающего события</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="166"/>
+        <location filename="rulesChecker.cpp" line="167"/>
         <source>Some links are incorrect</source>
         <translation>Поток управления без начала или конца</translation>
     </message>
     <message>
-        <location filename="rulesChecker.cpp" line="170"/>
+        <location filename="rulesChecker.cpp" line="171"/>
         <source>There are problems</source>
         <translation>Неизвестная ошибка</translation>
     </message>
