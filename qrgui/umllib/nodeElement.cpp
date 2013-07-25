ad12193c55e3c99f26014b11768aa622e322996f
Ashatta <tatjana.agapova@gmail.com>
2013-07-25 15:01:41 +0400

    subprocess + icon

--- qrgui/umllib/nodeElement.cpp
+++ qrgui/umllib/nodeElement.cpp
@@ -1376,7 +1376,7 @@ QRectF NodeElement::diagramRenderingRect() const
 	qreal yCoeff = (boundingRect().height() - 3 * kvadratik) / (initial->boundingRect().height() - 3 *kvadratik);
 
 	// QReal:BP hardcode
-	QRectF result(QPointF(25 * xCoeff, 25 * yCoeff), QPointF(185 * xCoeff, 135 * yCoeff));
+	QRectF result(QPointF(25 * xCoeff, 25 * yCoeff), QPointF(185 * xCoeff, 115 * yCoeff));
 
 	QPointF oldCenter(result.center());
 	result.setSize(mRenderedDiagram.size().scaled(result.size().toSize(), Qt::KeepAspectRatio));
