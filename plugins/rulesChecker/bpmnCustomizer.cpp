8ba46066e29bd4bfdd27ff213d11a75db95582b8
Timofey Bryksin <timofey.bryksin@gmail.com>
2013-08-02 17:02:44 +0400

    updated changelog, changed version to 0.3.1

--- plugins/rulesChecker/bpmnCustomizer.cpp
+++ plugins/rulesChecker/bpmnCustomizer.cpp
@@ -17,7 +17,7 @@ QString BpmnCustomizer::windowTitle() const
 
 QString BpmnCustomizer::productVersion() const
 {
-	return "0.3.0";
+	return "0.3.1";
 }
 
 bool BpmnCustomizer::showConnectionRelatedMenus() const
