8b0cb8a922134938ea2f2fd2b533a3e72311aaf7
Dmitry Mordvinov <dvvsrd@gmail.com>
2013-09-05 16:05:38 +0400

    Added bool properties genration

--- plugins/robots/robotsGenerator/base/converters/boolPropertyConverter.cpp
+++ plugins/robots/robotsGenerator/base/converters/boolPropertyConverter.cpp
@@ -2,6 +2,28 @@
 
 using namespace qReal::robots::generators::converters;
 
-BoolPropertyConverter::BoolPropertyConverter()
+BoolPropertyConverter::BoolPropertyConverter(QString const &pathToTemplates
+		, simple::Binding::ConverterInterface const *inputPortConverter
+		, simple::Binding::ConverterInterface const *outputPortConverter
+		, simple::Binding::ConverterInterface const *functionInvocationsConverter
+		, bool needInverting)
+	: CodeConverterBase(pathToTemplates
+			, inputPortConverter
+			, outputPortConverter
+			, functionInvocationsConverter)
+	, mNeedInverting(needInverting)
 {
+}
+
+QString BoolPropertyConverter::convert(QString const &data) const
+{
+	QString const preparedCode = convert(data);
+	return mNeedInverting ? invert(preparedCode) : preparedCode;
+}
+
+QString BoolPropertyConverter::invert(QString const &expression) const
+{
+	QString invertTemplate = readTemplate("conditional/negation.t");
+	invertTemplate.replace("@@CONDITION@@", expression);
+	return invertTemplate;
 }
--- plugins/robots/robotsGenerator/base/converters/boolPropertyConverter.h
+++ plugins/robots/robotsGenerator/base/converters/boolPropertyConverter.h
@@ -1,14 +1,27 @@
 #pragma once
 
+#include "codeConverterBase.h"
+
 namespace qReal {
 namespace robots {
 namespace generators {
 namespace converters {
 
-class BoolPropertyConverter
+class BoolPropertyConverter : public CodeConverterBase
 {
 public:
-	BoolPropertyConverter();
+	BoolPropertyConverter(QString const &pathToTemplates
+			, simple::Binding::ConverterInterface const *inputPortConverter
+			, simple::Binding::ConverterInterface const *outputPortConverter
+			, simple::Binding::ConverterInterface const *functionInvocationsConverter
+			, bool needInverting);
+
+	virtual QString convert(QString const &data) const;
+
+private:
+	QString invert(QString const &expression) const;
+
+	bool mNeedInverting;
 };
 
 }
