b96c9209198377c324736da1f78ca3f6481f0c89
Dmitry Mordvinov <dvvsrd@gmail.com>
2013-12-04 19:47:43 +0400

    Part 5.7: First attempt to make travis happy

--- qrutils/mathUtils/math.h
+++ qrutils/mathUtils/math.h
@@ -6,10 +6,12 @@
 
 namespace mathUtils {
 
+/// Contains useful methods for using in some mathematical expressions
 class QRUTILS_EXPORT Math
 {
 public:
-	static const qreal EPS = 0.00001;
+	/// Default precision for all
+	static qreal const QRUTILS_EXPORT EPS = 0.00001;
 
 	/// Compares given numbers with default precision
 	static bool eq(qreal x, qreal y);
@@ -23,9 +25,13 @@ public:
 	/// Returns if x greater or equal to y
 	static bool geq(qreal x, qreal y);
 
+	/// Returns the square of x (x * x)
 	static int sqr(int x);
+
+	/// Returns the square of x (x * x)
 	static qreal sqr(qreal x);
 
+	/// Returns the sign of x (-1, 0 or 1)
 	static int sign(qreal x);
 
 	/// Returns if border1 >= value >= border2 or border1 <= value <= border2
