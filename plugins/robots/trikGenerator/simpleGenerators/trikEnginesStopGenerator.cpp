c6c1abc66bc315ba8976f4a8c151d20c23b32128
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-03-18 18:04:47 +0400

    Restored engines stop generator for TRIK

--- plugins/robots/trikGenerator/simpleGenerators/trikEnginesStopGenerator.cpp
+++ plugins/robots/trikGenerator/simpleGenerators/trikEnginesStopGenerator.cpp
@@ -7,7 +7,7 @@ TrikEnginesStopGenerator::TrikEnginesStopGenerator(qrRepo::RepoApi const &repo
 		, GeneratorCustomizer &customizer
 		, Id const &id
 		, QObject *parent)
-	: BindingGenerator(repo, customizer, id, "engines/power/stop.t", QList<Binding *>()
+	: BindingGenerator(repo, customizer, id, "engines/stop.t", QList<Binding *>()
 			<< Binding::createMultiTarget("@@PORT@@", "Ports", customizer.factory()->enginesConverter())
 			, parent)
 {
