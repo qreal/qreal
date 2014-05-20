d8c73020dd260089eb59e206dec771d0e86b5e3a
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-05-20 14:34:36 +0400

    IR sensor`s behavior made more realistic

--- plugins/robots/interpreters/trikKitInterpreter/src/robotModel/twoD/parts/twoDInfraredSensor.cpp
+++ plugins/robots/interpreters/trikKitInterpreter/src/robotModel/twoD/parts/twoDInfraredSensor.cpp
@@ -4,6 +4,10 @@
 
 using namespace trikKitInterpreter::robotModel::twoD::parts;
 
+/// The maximal distance that returned by IR sensor; when the object is closer
+/// IR sensor starts to return less values non-lineary.
+int const maxDistance = 93;
+
 TwoDInfraredSensor::TwoDInfraredSensor(interpreterBase::robotModel::DeviceInfo const &info
 		, interpreterBase::robotModel::PortInfo const &port
 		, twoDModel::engine::TwoDModelEngineInterface &engine)
@@ -14,7 +18,10 @@ TwoDInfraredSensor::TwoDInfraredSensor(interpreterBase::robotModel::DeviceInfo c
 void TwoDInfraredSensor::read()
 {
 	int const sonarDistanceValue = mEngine.readSonarSensor(port());
-	int const linearDistance = 100 - sonarDistanceValue * 100 / 255;
-	/// @todo: Know how real IR sensor works and model something similar to it.
-	emit newData(linearDistance);
+	int const linearDistance = 100 - (sonarDistanceValue < 100 ? sonarDistanceValue : 100);
+	int const spoiledDistance = linearDistance <= maxDistance
+			? linearDistance
+			// On small distances IR sensor starts to work non-lineary
+			: 100 - (linearDistance - maxDistance) * 10;
+	emit newData(spoiledDistance);
 }
