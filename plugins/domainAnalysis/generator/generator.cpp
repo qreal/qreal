818fe1b1c06d63e92fe20162c4c44ca27dd92f93
AnnaGudoshnikova <goodanna_13@mail.ru>
2014-06-01 22:57:33 +0400

    generator has updated

--- plugins/domainAnalysis/generator/generator.cpp
+++ plugins/domainAnalysis/generator/generator.cpp
@@ -8,17 +8,13 @@
 #include <QtCore/QTextStream>
 #include <QtCore/QFile>
 
-#include "../../../qrkernel/settingsManager.h"
-#include "../../../qrmc/metaCompiler.h"
-
-
-#include "../../../qrkernel/roles.h"
-
-#include "../../../qrutils/outFile.h"
-#include "../../../qrutils/nameNormalizer.h"
-
-#include "../../../qrkernel/settingsManager.h"
-#include "../../../qrgui/pluginManager/editorManager.h"
+#include "qrkernel/settingsManager.h"
+#include "qrmc/metaCompiler.h"
+#include "qrkernel/roles.h"
+#include "qrutils/outFile.h"
+#include "qrutils/nameNormalizer.h"
+#include "qrkernel/settingsManager.h"
+#include "qrgui/pluginManager/editorManager.h"
 
 using namespace qReal;
 using namespace domainAnalysis;
@@ -27,8 +23,8 @@ using namespace qrRepo;
 
 
 generator::generator()
-		: mGenerateMetamodelAction(NULL)
-		, mRepoControlApi(NULL)
+		: mGenerateMetamodelAction(nullptr)
+		, mRepoControlApi(nullptr)
 {
 }
 
@@ -56,66 +52,43 @@ QList<ActionInfo> generator::actions()
 void generator::onGenerateMetamodel()
 {
 
-	QFile f("D:\\test.txt");
+	QFile testFile("D:\\test.txt");
 	mRepo = new qrRepo::RepoApi("testRepo.qrs");
-
-	//testId - узел редактора
+	//testId - editorNode
 	Id const editorTestId("MetaEditor", "MetaEditor", "MetamodelDiagram", QUuid::createUuid().toString());
 	mRepo->addChild(Id::rootId(), editorTestId);
 	mRepo->setProperty(editorTestId,"name", "Editor");
 	mRepo->setProperty(editorTestId,"displayedName", "Editor");
-	//mRepo->saveAll();
-	//узел диаграммы
+	//DiagramNode
 	Id const diagramNode("MetaEditor", "MetaEditor", "MetaEditorDiagramNode", QUuid::createUuid().toString());
 	mRepo->addChild(editorTestId, diagramNode);
 	mRepo->setProperty(diagramNode,"name", "Diagram");
 	mRepo->setProperty(diagramNode,"displayedName", "Diagram");
 	mRepo->setProperty(diagramNode,"nodeName", "Diagram");
-
-	if (f.open(QIODevice::WriteOnly))
-	{
-		QTextStream stream(&f);
+	if (testFile.open(QIODevice::WriteOnly)){
+		QTextStream stream(&testFile);
 		IdList const metamodels = mLogicalRepoApi->children(Id::rootId());
-		foreach (Id const key, metamodels)
-		{
-
+		for (Id const key: metamodels){
 			QString const objectType = key.element();
-
-			if (objectType == "DetailedFeature" && mLogicalRepoApi->isLogicalElement(key))
-			{
+			if (objectType == "DetailedFeature" && mLogicalRepoApi->isLogicalElement(key)){
 				stream << mLogicalRepoApi->name(key)<<"\r\n";
 				Id const nodeId("MetaEditor", "MetaEditor", "MetaEntityNode", QUuid::createUuid().toString());
 				mRepo->addChild(diagramNode,nodeId);
 				QString const &name = mLogicalRepoApi->name(key);
 				mRepo->setProperty(nodeId,"name", name);
 				mRepo->setProperty(nodeId, "displayedName", name);
-
-				//mRepo->setProperty(nodeId,"shape",mLogicalRepoApi->stringProperty(key, "shape"));
-
 			}
-
 		}
-
 		Id const edgeId("MetaEditor", "MetaEditor", "MetaEntityEdge", QUuid::createUuid().toString());
 		mRepo->addChild(diagramNode,edgeId);
 		mRepo->setProperty(edgeId,"name","Стрелка следования");
 		mRepo->setProperty(edgeId, "displayedName", "Стрелка следования");
 		mRepo->setProperty(edgeId,"shape", "broken");
 		mRepo->setProperty(edgeId,"lineType", "solidLine");
-
-
 		mRepo->saveTo("D:/testEditor.qrs");
-
-
-		f.close();
-		if (stream.status() != QTextStream::Ok)
-		{
+		testFile.close();
+		if (stream.status() != QTextStream::Ok)	{
 			qDebug() << "Ошибка записи файла";
 		}
-
-		//qrRepo::RepoApi *repoApi = new qrRepo::RepoApi(workingCopy);
-
 	}
-
-
 }
