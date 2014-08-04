4de3ca3283e0b11a1519a65506c79f9ef37d2806
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-08-04 15:41:41 +0400

    Fixed tones of encoder sensor reading connections while waiting it

--- plugins/robots/interpreters/interpreterBase/include/interpreterBase/blocksBase/common/waitForEncoderBlock.h
+++ plugins/robots/interpreters/interpreterBase/include/interpreterBase/blocksBase/common/waitForEncoderBlock.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <interpreterBase/blocksBase/common/waitBlock.h>
+#include <interpreterBase/blocksBase/common/waitForSensorBlock.h>
 
 namespace interpreterBase {
 namespace robotModel {
@@ -14,7 +14,7 @@ namespace common {
 
 /// An interpreter`s implementation for "wait for the encoder" block.
 /// @todo Inherit it from WaitForSensorBlock?
-class ROBOTS_INTERPRETER_BASE_EXPORT WaitForEncoderBlock : public WaitBlock
+class ROBOTS_INTERPRETER_BASE_EXPORT WaitForEncoderBlock : public WaitForSensorBlock
 {
 	Q_OBJECT
 
@@ -22,18 +22,12 @@ public:
 	/// Constructor, takes current robot model as parameter.
 	explicit WaitForEncoderBlock(robotModel::RobotModelInterface &robotModel);
 
-	void run() override;
-
 private slots:
 	/// Called when new data from encoder ready, checks it against "TachoLimit" property.
 	void responseSlot(int reading);
 
-	void timerTimeout() override;
-
 private:
 	robotModel::DeviceInfo device() const override;
-
-	robotModel::robotParts::EncoderSensor *mEncoderSensor;  // Does not have ownership
 };
 
 }
