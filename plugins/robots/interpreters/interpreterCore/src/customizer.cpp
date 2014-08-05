dcb549f1f80cd8ca530c49457703a5dfe1c24e92
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-08-05 19:18:44 +0400

    Sensors plotter enabled back

--- plugins/robots/interpreters/interpreterCore/src/customizer.cpp
+++ plugins/robots/interpreters/interpreterCore/src/customizer.cpp
@@ -60,12 +60,11 @@ void Customizer::placeWatchPlugins(QDockWidget *watchWindow, QWidget *graphicsWa
 {
 	mDockInterface->addDockWidget(Qt::LeftDockWidgetArea, watchWindow);
 	watchWindow->setFloating(false);
-	/// @todo: Restore plotter when everything will be fine...
-	Q_UNUSED(graphicsWatch)
-	// QDockWidget *graphWatchDock = produceDockWidget(QObject::tr("Sensors state"), graphicsWatch);
-	// mDockInterface->addDockWidget(Qt::LeftDockWidgetArea, graphWatchDock);
 
-	// mDockInterface->tabifyDockWidget(watchWindow, graphWatchDock);
+	QDockWidget *graphWatchDock = produceDockWidget(QObject::tr("Sensors state"), graphicsWatch);
+	mDockInterface->addDockWidget(Qt::LeftDockWidgetArea, graphWatchDock);
+
+	mDockInterface->tabifyDockWidget(watchWindow, graphWatchDock);
 }
 
 QDockWidget *Customizer::produceDockWidget(QString const &title, QWidget *content) const
