f533e627930f4d716e75a57f815bc1320ce6e421
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-08-15 03:08:18 +0400

    Added some labels translations to russian

--- installer/packages/com.qreal.root/meta/prebuild.sh
+++ installer/packages/com.qreal.root/meta/prebuild.sh
@@ -4,13 +4,13 @@ set -o errexit
 
 cd "$(dirname "$0")"
 
-cp     $INSTALLER_ROOT/licenses/gpl-3.0.txt                             $PWD
-cp     $INSTALLER_ROOT/licenses/OFL.txt                                 $PWD
-cp     $INSTALLER_ROOT/licenses/QsKineticScroller-license.txt           $PWD
-cp     $INSTALLER_ROOT/licenses/QsLog-license.txt                       $PWD
-cp     $INSTALLER_ROOT/licenses/QtPropertyBrowser-license.txt           $PWD
-cp     $INSTALLER_ROOT/licenses/Scintilla-license.txt                   $PWD
-cp     $INSTALLER_ROOT/licenses/WindowsModernStyle-license.txt          $PWD
+cp     $INSTALLER_ROOT/licenses/gpl-3.0*.txt                             $PWD
+cp     $INSTALLER_ROOT/licenses/OFL*.txt                                 $PWD
+cp     $INSTALLER_ROOT/licenses/QsKineticScroller-license*.txt           $PWD
+cp     $INSTALLER_ROOT/licenses/QsLog-license*.txt                       $PWD
+cp     $INSTALLER_ROOT/licenses/QtPropertyBrowser-license*.txt           $PWD
+cp     $INSTALLER_ROOT/licenses/Scintilla-license*.txt                   $PWD
+cp     $INSTALLER_ROOT/licenses/WindowsModernStyle-license*.txt          $PWD
 
 
 mkdir -p $PWD/../data
