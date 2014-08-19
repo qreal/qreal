1e62ab43ab3ffb73c40de673549447d389153e37
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-08-19 17:55:25 +0400

    Components structure made more useful

--- installer/build-trik-studio.sh
+++ installer/build-trik-studio.sh
@@ -7,5 +7,5 @@ cd "$(dirname "$0")"
 
 # Usage: build-trik-studio.sh <path-to-qt>. See build-installer.sh for more info.
 
-[ $OSTYPE == "linux-gnu" ] && EXCLUDE="-e com.qreal.root.robots.nxt_tools,com.qreal.root.associations" || EXCLUDE=
+[ $OSTYPE == "linux-gnu" ] && EXCLUDE="-e com.qreal.root.nxt_tools,com.qreal.root.associations" || EXCLUDE=
 chmod +x $PWD/build-installer.sh && $PWD/build-installer.sh $@ trik-studio $EXCLUDE
