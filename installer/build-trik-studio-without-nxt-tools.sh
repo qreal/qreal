a186e96b8f2a08250b9ec7c66c5c2af011939b1f
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-10-15 17:41:17 +0400

    Added nxt-tools support in installer

--- installer/build-trik-studio-without-nxt-tools.sh
+++ installer/build-trik-studio-without-nxt-tools.sh
@@ -7,5 +7,5 @@ cd "$(dirname "$0")"
 
 # Usage: build-trik-studio.sh <path-to-qt> <path-to-qt-installer-framework>. See build-installer.sh for more info.
 
-[ $OSTYPE == "linux-gnu" ] && EXCLUDE="-e com.qreal.root.nxt_tools,com.qreal.root.associations" || EXCLUDE=
+[ $OSTYPE == "linux-gnu" ] && EXCLUDE="-e com.qreal.root.nxt_tools,com.qreal.root.associations" || EXCLUDE="-e com.qreal.root.nxt_tools"
 chmod +x $PWD/build-installer.sh && $PWD/build-installer.sh $@ trik-studio $EXCLUDE
