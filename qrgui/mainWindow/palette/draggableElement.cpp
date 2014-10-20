a1ddef8089c9da2295fd8922bcedd92798ef43d1
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-10-20 18:32:32 +0400

    Added forgotten modifications

--- qrgui/mainWindow/palette/draggableElement.cpp
+++ qrgui/mainWindow/palette/draggableElement.cpp
@@ -123,7 +123,7 @@ void DraggableElement::changeAppearancePaletteActionTriggered()
 	QAction const * const action = static_cast<QAction *>(sender());
 	Id const id = action->data().value<Id>();
 	QString const propertyValue = mEditorManagerProxy.shape(id);
-	mMainWindow.openShapeEditor(id, propertyValue, mEditorManagerProxy, false);
+	mMainWindow.openShapeEditor(id, propertyValue, &mEditorManagerProxy, false);
 }
 
 void DraggableElement::deleteElementPaletteActionTriggered()
