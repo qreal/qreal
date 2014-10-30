ca9275db22e27ae503c76d0543f95c1553c9ae7c
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-10-30 03:35:23 +0300

    Updated translations for new libs

---
+++ qrtranslations/ru/plugins/robots/nxtRussianCGenerator_ru.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>nxt::russianC::NxtRussianCGeneratorPlugin</name>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtRussianCGenerator/nxtRussianCGeneratorPlugin.cpp" line="27"/>
+        <source>RussianC Source File</source>
+        <translation>Файл с исходным кодом на Школьном Алгоритмическом Языке</translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtRussianCGenerator/nxtRussianCGeneratorPlugin.cpp" line="37"/>
+        <source>Generate to Russian C</source>
+        <translation>Генерировать на Школьном Алгоритмическом Языке</translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtRussianCGenerator/nxtRussianCGeneratorPlugin.cpp" line="50"/>
+        <source>Generate Russian C Code</source>
+        <translation>Сгенерировать код на Школьном Алгоритмическом Языке</translation>
+    </message>
+</context>
+</TS>
