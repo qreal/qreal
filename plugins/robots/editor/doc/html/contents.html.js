90f5f52c375f01195925d61c4008785db1132d12
Yurii Litvinov <yurii.litvinov@gmail.com>
2014-11-17 00:34:22 +0300

    updated index and interface description

---
+++ plugins/robots/editor/doc/html/contents.html.js
@@ -0,0 +1,55 @@
+document.write('\
+	<div> \
+		<h2>Содержание:</h2>\
+		<ul class="list-style2">\
+			<li><a href="interface.html">Пользовательский интерфейс</a>\
+				<ul>\
+					<li><a href="interface.html#overview">Обзор</li>\
+					<li><a href="interface.html#settings">Диалог настроек</li>\
+					<li><a href="interface.html#d2model">Диалог двумерной модели робота</li>\
+				</ul>\
+			</li>\
+			<li><a href="programming.html">Создание программ</a>\
+				<ul>\
+					<li><a href="programming.html#diagrams">Создание диаграмм</a></li>\
+					<li><a href="programming.html#blocks">Создание блоков</a></li>\
+					<li><a href="programming.html#links">Создание связей</a></li>\
+					<li><a href="programming.html#functions">Синтаксис выражений</a></li>\
+					<li><a href="programming.html#properties">Редактирование свойств элементов</a></li>\
+				</ul>\
+			</li>\
+			<li>ТРИК\
+				<ul>\
+					<li><a href="trik-robot-setup.html">Подключение робота</a></li>\
+					<li><a href="trik-additional-blocks.html">Специальные блоки</a></li>\
+					<li><a href="trik-run-programs.html">Запуск программ</a></li>\
+					<ul>\
+						<li><a href="trik-run-programs.html#wifi">Интерпретация по Wi-Fi</a></li>\
+						<li><a href="trik-run-programs.html#uploading">Загрузка программы на робот (Qts)</a></li>\
+						<li><a href="trik-run-programs.html#uploading">Загрузка программы на робот (F#)</a></li>\
+					</ul>\
+				</ul>\
+			</li>\
+			<li>Lego NXT\
+				<ul>\
+					<li><a href="nxt-robot-setup.html">Подключение робота</a></li>\
+					<li><a href="nxt-robot-setup.html">Специальные блоки</a></li>\
+					<li><a href="nxt-run-programs.html">Запуск программ</a></li>\
+					<ul>\
+						<li><a href="nxt-run-programs.html#bluetooth">Интерпретация по Bluetooth</a></li>\
+						<li><a href="nxt-run-programs.html#flashing">Прошивка в робота</a></li>\
+					</ul>\
+				</ul>\
+			</li>\
+			<li>Lego EV3\
+				<ul>\
+					<li><a href="ev3-robot-setup.html">Подключение робота</a></li>\
+					<li><a href="ev3-run-programs.html">Запуск программ</a></li>\
+					<ul>\
+						<li><a href="ev3-run-programs.html#bluetooth">Интерпретация по Bluetooth</a></li>\
+					</ul>\
+				</ul>\
+			</li>\
+		</ul>\
+	</div>\
+');
