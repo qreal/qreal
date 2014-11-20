a6b33c0531c1a6318739742719557beb44f0af8e
Smirnov Mikhail <sm_m_a@mail.ru>
2014-11-21 02:55:52 +0300

    Add range sensor. Color and light sensors not work.

---
+++ plugins/robots/interpreters/ev3KitInterpreter/src/robotModel/real/parts/colorSensorFull.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <interpreterBase/robotModel/robotParts/colorSensorFull.h>
+#include <utils/robotCommunication/robotCommunicator.h>
+
+#include "implementations/colorSensorImpl.h"
+
+namespace ev3KitInterpreter {
+namespace robotModel {
+namespace real {
+namespace parts {
+
+class ColorSensorFull : public interpreterBase::robotModel::robotParts::ColorSensorFull
+{
+    Q_OBJECT
+
+public:
+    ColorSensorFull(interpreterBase::robotModel::DeviceInfo const &info
+              , interpreterBase::robotModel::PortInfo const &port
+              , utils::robotCommunication::RobotCommunicator &robotCommunicator);
+
+    void read() override;
+
+private:
+    ColorSensorImpl mColorSensor;
+    utils::robotCommunication::RobotCommunicator &mRobotCommunicator;
+};
+
+}
+}
+}
+}
