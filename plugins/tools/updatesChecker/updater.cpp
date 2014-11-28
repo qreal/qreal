92fc572f7950e8b1e0b9e3d866fb337a5b85e097
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-11-28 21:38:26 +0300

    Fixed false update promotions

--- plugins/tools/updatesChecker/updater.cpp
+++ plugins/tools/updatesChecker/updater.cpp
@@ -44,8 +44,8 @@ void Updater::readAnswer()
 	// Checking that output is a valid XML
 	QDomDocument parser;
 	parser.setContent(output);
-	if (!output.isEmpty() && !parser.isNull()) {
-		QLOG_INFO() << "Updater output:" << output;
+	QLOG_INFO() << "Updater output:" << output;
+	if (!output.isEmpty() && !parser.isNull() && output.trimmed().startsWith("<")) {
 		emit newVersionAvailable();
 	} else {
 		emit noNewVersionAvailable();
