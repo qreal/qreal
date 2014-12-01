d03e8f7c73c8fb73e81e58a8a779ad64f13a99e5
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-12-01 14:50:51 +0300

    Added translations for Qt Property Browser Framework

---
+++ qrtranslations/ru/qrgui_thirdparty_ru.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>QtBoolEdit</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="233"/>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="243"/>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="268"/>
+        <source>True</source>
+        <translation>Истина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="243"/>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="268"/>
+        <source>False</source>
+        <translation>Ложь</translation>
+    </message>
+</context>
+<context>
+    <name>QtBoolPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="1678"/>
+        <source>True</source>
+        <translation>Истина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="1679"/>
+        <source>False</source>
+        <translation>Ложь</translation>
+    </message>
+</context>
+<context>
+    <name>QtCharEdit</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qteditorfactory.cpp" line="1675"/>
+        <source>Clear Char</source>
+        <translation>Очистить</translation>
+    </message>
+</context>
+<context>
+    <name>QtColorEditWidget</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qteditorfactory.cpp" line="2346"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+</context>
+<context>
+    <name>QtColorPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6455"/>
+        <source>Red</source>
+        <translation>Красный</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6463"/>
+        <source>Green</source>
+        <translation>Зеленый</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6471"/>
+        <source>Blue</source>
+        <translation>Синий</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6479"/>
+        <source>Alpha</source>
+        <translation>Прозрачность (Альфа)</translation>
+    </message>
+</context>
+<context>
+    <name>QtCursorDatabase</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="58"/>
+        <source>Arrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="60"/>
+        <source>Up Arrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="62"/>
+        <source>Cross</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="64"/>
+        <source>Wait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="66"/>
+        <source>IBeam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="68"/>
+        <source>Size Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="70"/>
+        <source>Size Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="72"/>
+        <source>Size Backslash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="74"/>
+        <source>Size Slash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="76"/>
+        <source>Size All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="78"/>
+        <source>Blank</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="80"/>
+        <source>Split Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="82"/>
+        <source>Split Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="84"/>
+        <source>Pointing Hand</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="86"/>
+        <source>Forbidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="88"/>
+        <source>Open Hand</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="90"/>
+        <source>Closed Hand</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="92"/>
+        <source>What&apos;s This</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="94"/>
+        <source>Busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QtFontEditWidget</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qteditorfactory.cpp" line="2555"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qteditorfactory.cpp" line="2575"/>
+        <source>Select Font</source>
+        <translation>Выберите шрифт</translation>
+    </message>
+</context>
+<context>
+    <name>QtFontPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6129"/>
+        <source>Family</source>
+        <translation>Семейство</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6142"/>
+        <source>Point Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6150"/>
+        <source>Bold</source>
+        <translation>Жирный</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6157"/>
+        <source>Italic</source>
+        <translation>Курсив</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6164"/>
+        <source>Underline</source>
+        <translation>Подчеркнутый</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6171"/>
+        <source>Strikeout</source>
+        <translation>Перечеркнутый</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="6178"/>
+        <source>Kerning</source>
+        <translation>Кернинг</translation>
+    </message>
+</context>
+<context>
+    <name>QtKeySequenceEdit</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="328"/>
+        <source>Clear Shortcut</source>
+        <translation>Очистить</translation>
+    </message>
+</context>
+<context>
+    <name>QtLocalePropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2565"/>
+        <source>%1, %2</source>
+        <translation>%1, %2</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2618"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2626"/>
+        <source>Country</source>
+        <translation>Страна</translation>
+    </message>
+</context>
+<context>
+    <name>QtPointFPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3035"/>
+        <source>(%1, %2)</source>
+        <translation>(%1, %2)</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3106"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3114"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+</context>
+<context>
+    <name>QtPointPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2795"/>
+        <source>(%1, %2)</source>
+        <translation>(%1, %2)</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2832"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="2839"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+</context>
+<context>
+    <name>QtPropertyBrowserUtils</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="187"/>
+        <source>[%1, %2, %3] (%4)</source>
+        <translation>[%1, %2, %3] (%4)</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertybrowserutils.cpp" line="214"/>
+        <source>[%1, %2]</source>
+        <translation>[%1, %2]</translation>
+    </message>
+</context>
+<context>
+    <name>QtRectFPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4540"/>
+        <source>[(%1, %2), %3 x %4]</source>
+        <translation>[(%1, %2), %3 x %4]</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4696"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4704"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4712"/>
+        <source>Width</source>
+        <translation>Ширина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4721"/>
+        <source>Height</source>
+        <translation>Высота</translation>
+    </message>
+</context>
+<context>
+    <name>QtRectPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4110"/>
+        <source>[(%1, %2), %3 x %4]</source>
+        <translation>[(%1, %2), %3 x %4]</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4230"/>
+        <source>X</source>
+        <translation>X</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4237"/>
+        <source>Y</source>
+        <translation>Y</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4244"/>
+        <source>Width</source>
+        <translation>Ширина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="4252"/>
+        <source>Height</source>
+        <translation>Высота</translation>
+    </message>
+</context>
+<context>
+    <name>QtSizeFPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3718"/>
+        <source>%1 x %2</source>
+        <translation>%1 x %2</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3848"/>
+        <source>Width</source>
+        <translation>Ширина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3857"/>
+        <source>Height</source>
+        <translation>Высота</translation>
+    </message>
+</context>
+<context>
+    <name>QtSizePolicyPropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5641"/>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5642"/>
+        <source>&lt;Invalid&gt;</source>
+        <translation>&lt;Некорректное значение&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5643"/>
+        <source>[%1, %2, %3, %4]</source>
+        <translation>[%1, %2, %3, %4]</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5688"/>
+        <source>Horizontal Policy</source>
+        <translation>Горизонтальная политика</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5697"/>
+        <source>Vertical Policy</source>
+        <translation>Вертикальная политика</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5706"/>
+        <source>Horizontal Stretch</source>
+        <translation>Растяжимость по горизонтали</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="5714"/>
+        <source>Vertical Stretch</source>
+        <translation>Растяжимость по вертикали</translation>
+    </message>
+</context>
+<context>
+    <name>QtSizePropertyManager</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3354"/>
+        <source>%1 x %2</source>
+        <translation>%1 x %2</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3450"/>
+        <source>Width</source>
+        <translation>Ширина</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qtpropertymanager.cpp" line="3458"/>
+        <source>Height</source>
+        <translation>Высота</translation>
+    </message>
+</context>
+<context>
+    <name>QtTreePropertyBrowser</name>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qttreepropertybrowser.cpp" line="477"/>
+        <source>Property</source>
+        <translation>Свойство</translation>
+    </message>
+    <message>
+        <location filename="../../qrgui/thirdparty/qtpropertybrowser/src/qttreepropertybrowser.cpp" line="478"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+</context>
+</TS>
