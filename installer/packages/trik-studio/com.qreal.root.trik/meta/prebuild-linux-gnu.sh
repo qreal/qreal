55520c52f80a564894a5a45016a5c2e276cedc58
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-12-19 01:37:21 +0300

    More fixes for linux installer builder

--- installer/packages/trik-studio/com.qreal.root.trik/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/com.qreal.root.trik/meta/prebuild-linux-gnu.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")"
 
 cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-interpreter.so                 $PWD/../data/plugins/tools/kitPlugins/
 cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-qts-generator.so               $PWD/../data/plugins/tools/kitPlugins/
-cp     $BIN_DIR/librobots-trik-generator-base.so                                       $PWD/../data/plugins/tools/kitPlugins/
+cp     $BIN_DIR/librobots-trik-generator-base.so                                       $PWD/../data/
 
 cp -pr $QT_DIR/lib/libQt5Network.so*                                                   $PWD/../data
