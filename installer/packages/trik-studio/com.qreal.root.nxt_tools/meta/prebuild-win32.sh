e367fbb4a235d7092c4f4a8e806155df94744a42
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-12-19 04:29:31 +0300

    Added warnings when no suiting tag found in dependencies

--- installer/packages/trik-studio/com.qreal.root.nxt_tools/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/com.qreal.root.nxt_tools/meta/prebuild-win32.sh
@@ -4,12 +4,15 @@ set -o errexit
 
 cd "$(dirname "$0")"
 
+
+function checkout_tag_or_warn { [[ $'\n'$(git tag)$'\n' =~ $'\n'$1$'\n' ]] && git checkout $1 || echo "Warning: NXT Tools doesn`t have tag "$1; }
+
 mkdir -p $PWD/../data/
 
 git config --global core.autocrlf input
 cd $PWD/../data
 rm -rf nxt-tools
 git clone https://github.com/qreal/nxt-tools.git
-[ -z "${QREAL_BUILD_TAG:-}" ] && : || cd nxt-tools && git checkout $QREAL_BUILD_TAG
+[ -z "${QREAL_BUILD_TAG:-}" ] && : || cd nxt-tools && checkout_tag_or_warn $QREAL_BUILD_TAG
 rm -rf nxt-tools/.git
 git config --global core.autocrlf true
