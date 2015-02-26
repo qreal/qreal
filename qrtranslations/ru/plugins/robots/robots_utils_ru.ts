243e0b66bec3bd1bd69ed5979e2bc049b975a49f
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-02-26 19:32:39 +0300

    - made some error messages more clear - network configuration diagram example

--- qrtranslations/ru/plugins/robots/robots_utils_ru.ts
+++ qrtranslations/ru/plugins/robots/robots_utils_ru.ts
@@ -4,12 +4,25 @@
 <context>
     <name>utils::TcpRobotCommunicator</name>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/tcpRobotCommunicator.cpp" line="120"/>
         <source>Current TRIK runtime version is not equal to version required by TRIKStudio</source>
-        <translation>Текущая версия прошивки ТРИК не соответствует версии, требуемой ТРИКСтудией</translation>
+        <translation type="vanished">Текущая версия прошивки ТРИК не соответствует версии, требуемой ТРИКСтудией</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/tcpRobotCommunicator.cpp" line="160"/>
+        <source>From robot:</source>
+        <translation type="vanished">С робота:</translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/tcpRobotCommunicator.cpp" line="116"/>
+        <source>From robot: </source>
+        <translation>На роботе: </translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/tcpRobotCommunicator.cpp" line="122"/>
+        <source>TRIK runtime version is too old, please update it by pressing &apos;Upload Runtime&apos; button on toolbar</source>
+        <translation>ПО робота устарело, обновите его, нажав на кнопку &quot;Загрузить ПО на робот&quot; на панели инструментов</translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/tcpRobotCommunicator.cpp" line="163"/>
         <source>Current TRIK runtime version can not be received</source>
         <translation>Не удаётся получить текущую версию прошивки ТРИК</translation>
     </message>
