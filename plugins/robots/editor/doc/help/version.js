86dfa70842c7f4187cc6ad0e79ed2737399043a6
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-02-26 20:17:31 +0300

    updated version, changelog

--- plugins/robots/editor/doc/help/version.js
+++ plugins/robots/editor/doc/help/version.js
@@ -1,8 +1,8 @@
-var version = "3.0.1";
+var version = "3.0.2";
 var year = 2015;
 
 window.onload = function() {
-	document.title = document.title.replace("%version%", version);
-	document.body.innerHTML = document.body.innerHTML.replace("%version%", version);
-	document.body.innerHTML = document.body.innerHTML.replace("%year%", year);
+    document.title = document.title.replace("%version%", version);
+    document.body.innerHTML = document.body.innerHTML.replace("%version%", version);
+    document.body.innerHTML = document.body.innerHTML.replace("%year%", year);
 }
