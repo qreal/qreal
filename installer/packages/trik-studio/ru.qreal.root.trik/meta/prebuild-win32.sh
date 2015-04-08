1642e27562a148c2be37272e1c1464237c0f714c
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-04-08 15:06:03 +0300

    Installer scripts modified due to new lib names

--- installer/packages/trik-studio/ru.qreal.root.trik/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik/meta/prebuild-win32.sh
@@ -9,6 +9,7 @@ cp    $BIN_DIR/plugins/tools/kitPlugins/robots-trik-interpreter.dll             
 cp    $BIN_DIR/plugins/tools/kitPlugins/robots-trik-qts-generator.dll             $PWD/../data/plugins/tools/kitPlugins/
 cp    $BIN_DIR/plugins/tools/kitPlugins/robots-trik-runtime-uploader-plugin.dll   $PWD/../data/plugins/tools/kitPlugins/
 cp    $BIN_DIR/robots-trik-generator-base.dll                                     $PWD/../data/
+cp    $BIN_DIR/robots-trik-kit.dll                                                $PWD/../data/
 
 
 cp    $QT_DIR/bin/Qt5Network.dll                                                  $PWD/../data
