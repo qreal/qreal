ecfa838824ca40705e29e0f6fff522411af70012
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-04-09 15:02:27 +0300

    Fixed BOM encoding in BPMN checker files

--- plugins/rulesChecker/rulesBPMNPlugin.cpp
+++ plugins/rulesChecker/rulesBPMNPlugin.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
-﻿#include "rulesBPMNPlugin.h"
+#include "rulesBPMNPlugin.h"
 
 #include <QtWidgets/QApplication>
 
--- plugins/rulesChecker/rulesChecker.cpp
+++ plugins/rulesChecker/rulesChecker.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
-﻿#include "rulesChecker.h"
+#include "rulesChecker.h"
 #include <QtWidgets/QFileDialog>
 
 #include "../../qrrepo/repoApi.h"
