8d26ece2a00c4045768e65ac434c41d56c9ae96f
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-04-10 02:23:39 +0300

    Restored vera++ ignoring http links longer than 120 symbols in comments

--- buildScripts/vera++/scripts/rules/LineLength.tcl
+++ buildScripts/vera++/scripts/rules/LineLength.tcl
@@ -22,7 +22,7 @@ foreach fileName [getSourceFileNames] {
     set lineNumber 1
     foreach line [getAllLines $fileName] {
         set line [string map {"\t" "    "} $line]
-        if {[string length $line] > $maxLength} {
+        if {[string length $line] > $maxLength && ![regexp {http://} $line]} {
             report $fileName $lineNumber "line is longer than $maxLength characters"
         }
         incr lineNumber
