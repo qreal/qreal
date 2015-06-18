8d0f13652d4b0e6f432db7a513c8c626f8385fe1
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-06-18 06:47:45 +0300

    EV3 generator added into installer

--- installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-linux-gnu.sh
@@ -6,5 +6,9 @@ cd "$(dirname "$0")"
 
 
 cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-ev3-interpreter.so   $PWD/../data/plugins/tools/kitPlugins/
+cp     $BIN_DIR/plugnis/tools/kitPlugins/libev3-rbf-generator.so        $PWD/../data/plugins/tools/kitPlugins/
 cp -pr $BIN_DIR/librobots-ev3-kit.so*                                   $PWD/../data
+cp -pr $BIN_DIR/librobots-ev3-generator-base.so*                        $PWD/../data
 cp -pr $BIN_DIR/libqextserialport.so*                                   $PWD/../data
+
+cp -pr /usr/bin/libusb*                                                 $PWD/../data
