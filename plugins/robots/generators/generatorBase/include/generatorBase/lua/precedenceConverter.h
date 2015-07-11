cc7658ad8780e661ee37d945c5844ec0be520151
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-07-12 02:13:50 +0300

    Some more fixes for vera

--- plugins/robots/generators/generatorBase/include/generatorBase/lua/precedenceConverter.h
+++ plugins/robots/generators/generatorBase/include/generatorBase/lua/precedenceConverter.h
@@ -12,6 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. */
 
+#pragma once
+
 #include "precedenceConverterInterface.h"
 
 #include <qrtext/lua/luaAstVisitorInterface.h>
