bdb64b96493e31d8ea7728aa755115bfef172066
Kirill-NiK <nikolskiy.kirill.andreevich@gmail.com>
2015-08-06 12:07:09 +0400

    failure attempts

--- qrtest/unitTests/guiTests/testScripts/qrealScripts/editActionsFunctioning.js
+++ qrtest/unitTests/guiTests/testScripts/qrealScripts/editActionsFunctioning.js
@@ -4,8 +4,9 @@ api.changeWindow(mainWindow);
 api.wait(500);
 var newProject = api.ui().widget("QToolButton", "actionNewProject");
 assert(newProject != null);
-closeExpectedDialog(mainWindow, "Create project", 3000);
-//chooseExpectedDialogDiagram(mainWindow, "Create project", "Robot`s Behaviour Diagram", 3000);
+expectDialog(mainWindow, "Create project", "Robot`s Behaviour Diagram", 3000);
 api.cursor().moveTo(newProject, 400);
 api.cursor().leftButtonPress(newProject);
 api.cursor().leftButtonRelease(newProject, 400);
+
+
