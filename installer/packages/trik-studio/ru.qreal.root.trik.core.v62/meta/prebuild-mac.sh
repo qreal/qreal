7a2bc27a5e2834de61aa3101968522d7e6f53243
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-08-19 21:28:29 +0300

    Implemented TRIK Runtime uploading under Linux and Mac

--- installer/packages/trik-studio/ru.qreal.root.trik.core.v62/meta/prebuild-mac.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.core.v62/meta/prebuild-mac.sh
@@ -8,6 +8,8 @@ source $INSTALLER_ROOT/utils/mac_utils.sh
 
 fix_dependencies $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-interpreter.dylib     $QT_DIR/lib @executable_path/../../..
 fix_dependencies $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-qts-generator.dylib   $QT_DIR/lib @executable_path/../../..
+fix_dependencies $BIN_DIR/plugins/tools/librobots-trik-v62-runtime-uploader-plugin.dylib    $QT_DIR/lib @executable_path/../../..
 
-cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-interpreter.dylib              $PWD/../data/plugins/tools/kitPlugins/
-cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-qts-generator.dylib            $PWD/../data/plugins/tools/kitPlugins/
+cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-interpreter.dylib               $PWD/../data/plugins/tools/kitPlugins/
+cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-trik-v62-qts-generator.dylib             $PWD/../data/plugins/tools/kitPlugins/
+cp     $BIN_DIR/plugins/tools/librobots-trik-v62-runtime-uploader-plugin.dylib              $PWD/../data/plugins/tools/
