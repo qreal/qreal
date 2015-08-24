983703c3ae6b4e161c7ba96eef26680fd0aea58e
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-08-24 19:33:18 +0300

    Fixed incorrect 2D checker behaviour after stop

--- plugins/robots/common/twoDModel/src/engine/model/timeline.cpp
+++ plugins/robots/common/twoDModel/src/engine/model/timeline.cpp
@@ -60,21 +60,23 @@ void Timeline::onTimer()
 
 	for (int i = 0; i < ticksPerCycle; ++i) {
 		QCoreApplication::processEvents();
-		mTimestamp += timeInterval;
-		emit tick();
-		++mCyclesCount;
-		if (mCyclesCount >= mSpeedFactor) {
-			mTimer.stop();
-			mCyclesCount = 0;
-			const int msFromFrameStart = static_cast<int>(QDateTime::currentMSecsSinceEpoch() - mFrameStartTimestamp);
-			const int pauseBeforeFrameEnd = mFrameLength - msFromFrameStart;
-			if (pauseBeforeFrameEnd > 0) {
-				QTimer::singleShot(pauseBeforeFrameEnd - 1, this, SLOT(gotoNextFrame()));
-			} else {
-				gotoNextFrame();
-			}
+		if (mIsStarted) {
+			mTimestamp += timeInterval;
+			emit tick();
+			++mCyclesCount;
+			if (mCyclesCount >= mSpeedFactor) {
+				mTimer.stop();
+				mCyclesCount = 0;
+				const int msFromFrameStart = static_cast<int>(QDateTime::currentMSecsSinceEpoch() - mFrameStartTimestamp);
+				const int pauseBeforeFrameEnd = mFrameLength - msFromFrameStart;
+				if (pauseBeforeFrameEnd > 0) {
+					QTimer::singleShot(pauseBeforeFrameEnd - 1, this, SLOT(gotoNextFrame()));
+				} else {
+					gotoNextFrame();
+				}
 
-			return;
+				return;
+			}
 		}
 	}
 }
