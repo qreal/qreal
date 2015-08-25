c81df2799d04f7646ecd3319face56580acb58e4
Kirill-NiK <nikolskiy.kirill.andreevich@gmail.com>
2015-08-25 12:37:08 +0400

    changed tabSceneWorkScript.js

--- qrtest/unitTests/guiTests/testScripts/qrealScripts/tabSceneWorkScript.js
+++ qrtest/unitTests/guiTests/testScripts/qrealScripts/tabSceneWorkScript.js
@@ -3,5 +3,25 @@ assert(mainWindow != null);
 api.changeWindow(mainWindow);
 api.wait(500);
 
-var newProject = api.ui().widget("QToolButton", "actionNewProject");
-assert(newProject != null);
+var openInterpreted = api.ui().getStartButton("Open interpreted diagram");
+assert(openInterpreted != null);
+assert(api.ui().isEnabledAndVisible(openInterpreted));
+closeExpectedDialog(mainWindow, "Select file with metamodel to open", 1000);
+api.cursor().moveTo(openInterpreted, 50);
+api.cursor().leftButtonPress(openInterpreted);
+api.cursor().leftButtonRelease(openInterpreted);
+api.wait(2500);
+
+var openInterpreted = api.ui().getStartButton("Open interpreted diagram");
+assert(openInterpreted != null);
+assert(api.ui().isEnabledAndVisible(openInterpreted));
+var createInterpreted = api.ui().getStartButton("Create interpreted diagram");
+assert(createInterpreted != null);
+assert(api.ui().isEnabledAndVisible(createInterpreted));
+closeExpectedDialog(mainWindow, "Enter the diagram name:", 1000);
+printInExspectedActiveWindow(mainWindow, "Enter the diagram name:", "qwe", 1000)
+okExpectedDialog(mainWindow, "Enter the diagram name:", 1500);
+api.cursor().moveTo(createInterpreted, 50);
+api.cursor().leftButtonPress(createInterpreted);
+api.cursor().leftButtonRelease(createInterpreted);
+api.wait(2000);
