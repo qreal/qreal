a01425b848f37e248bf0810bcf1151f126513e07
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-12-12 22:03:09 +0300

    added installer rules for other existing metacase components

--- installer/packages/qreal-metacase/ru.qreal.root.metaeditor/meta/prebuild-common.sh
+++ installer/packages/qreal-metacase/ru.qreal.root.metaeditor/meta/prebuild-common.sh
@@ -5,7 +5,5 @@ set -o errexit
 cd "$(dirname "$0")"
 
 mkdir -p $PWD/../data/plugins/editors
-mkdir -p $PWD/../data/plugins/tools
-#cp -r  $BIN_DIR/help                                                    $PWD/../data
-#cp -r  $BIN_DIR/images                                                  $PWD/../data
+cp -r  $BIN_DIR/help                                                    $PWD/../data
 cp     $BIN_DIR/splashscreen.png                                        $PWD/../data
