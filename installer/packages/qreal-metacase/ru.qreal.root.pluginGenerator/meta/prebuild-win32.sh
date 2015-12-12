64f9afd49f1ba57254f31d79eeebedee27ec9974
Yurii Litvinov <yurii.litvinov@gmail.com>
2015-12-12 23:54:21 +0300

    Small corections

--- installer/packages/qreal-metacase/ru.qreal.root.pluginGenerator/meta/prebuild-win32.sh
+++ installer/packages/qreal-metacase/ru.qreal.root.pluginGenerator/meta/prebuild-win32.sh
@@ -5,3 +5,4 @@ set -o errexit
 cd "$(dirname "$0")"
 
 cp    $BIN_DIR/plugins/tools/metaEditorSupport.dll                        $PWD/../data/plugins/tools/
+cp    $BIN_DIR/qrmc.dll                                                   $PWD/../data/
