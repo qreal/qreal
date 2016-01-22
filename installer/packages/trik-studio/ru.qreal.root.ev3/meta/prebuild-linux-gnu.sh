355e6f8bad33136ef615e3b7aa863dfd8451bff5
Dmitry Mordvinov <dvvsrd@gmail.com>
2016-01-22 19:04:23 +0400

    Fixed installer build under linux and mac

--- installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.ev3/meta/prebuild-linux-gnu.sh
@@ -7,13 +7,16 @@ cd "$(dirname "$0")"
 
 mkdir -p $PWD/../data/lib/plugins/tools/kitPlugins
 cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-ev3-interpreter.so   $PWD/../data/lib/plugins/tools/kitPlugins/
-cp     $BIN_DIR/plugnis/tools/kitPlugins/libev3-rbf-generator.so        $PWD/../data/lib/plugins/tools/kitPlugins/
+cp     $BIN_DIR/plugins/tools/kitPlugins/librobots-ev3-rbf-generator.so $PWD/../data/lib/plugins/tools/kitPlugins/
 cp -pr $BIN_DIR/librobots-ev3-kit.so*                                   $PWD/../data/lib
 cp -pr $BIN_DIR/librobots-ev3-generator-base.so*                        $PWD/../data/lib
 cp -pr $BIN_DIR/libqextserialport.so*                                   $PWD/../data/lib
 
-cp -pr /usr/bin/libusb*                                                 $PWD/../data/lib
+cp -L  `dpkg -L libusb-1.0-0-dev | grep .so`                            $PWD/../data/lib/libusb-1.0.so
 
 # Cleaning up prebuild-common.sh results...
 rm -rf $PWD/../data/plugins/
 mv $PWD/../data/examples $PWD/../data/resources
+
+cd $PWD/../data/lib/
+ln -s libusb-1.0.so libusb-1.0.so.0
