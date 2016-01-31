b9c6245cd3d88fe03b4e2c2fd18596438b34829e
Dmitry Mordvinov <dvvsrd@gmail.com>
2016-02-01 00:59:23 +0300

    Fixed alignment in qreal-bp prebuild-win32.sh

--- installer/packages/qreal-bp/ru.qreal.root.bp/meta/prebuild-win32.sh
+++ installer/packages/qreal-bp/ru.qreal.root.bp/meta/prebuild-win32.sh
@@ -7,5 +7,5 @@ cd "$(dirname "$0")"
 cp    $BIN_DIR/plugins/tools/rulesChecker.dll                        $PWD/../data/plugins/tools/
 cp    $BIN_DIR/plugins/tools/updatesChecker.dll                      $PWD/../data/plugins/tools/
 
-cp    $BIN_DIR/plugins/editors/bpmn.dll                      	     $PWD/../data/plugins/editors/
+cp    $BIN_DIR/plugins/editors/bpmn.dll                              $PWD/../data/plugins/editors/
 cp    $BIN_DIR/plugins/editors/classDiagram.dll                      $PWD/../data/plugins/editors/
