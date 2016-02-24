2373a80813ea2f5026c3580e72360345d9559936
Dmitry Mordvinov <dvvsrd@gmail.com>
2016-02-24 17:36:04 +0300

    Added translations

--- qrtranslations/fr/plugins/robots/nxtOsekCGenerator_fr.ts
+++ qrtranslations/fr/plugins/robots/nxtOsekCGenerator_fr.ts
@@ -4,93 +4,155 @@
 <context>
     <name>nxt::NxtFlashTool</name>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="57"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="72"/>
         <source>Robot is already being flashed</source>
         <translation>Le robot est en train d&apos;être flashé</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="71"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="80"/>
+        <source>Firmware file not found in nxt-tools directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="87"/>
+        <source>Flashing robot is possible only by USB. Please switch to USB mode.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="97"/>
         <source>Firmware flash started. Please don&apos;t disconnect robot during the process</source>
         <translation>Le téléversement du firmware est commencé. S&apos;il vous plait, ne déconnectéz pas le robot jusqu&apos;à ce que le processus se termine</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="94"/>
         <source>Some error occured. Make sure you are running QReal with superuser privileges</source>
-        <translation>Une erreur s&apos;est produite. Assurez-vous que QReal est lancé avec les droits d&apos;un superutilisateur</translation>
+        <translation type="vanished">Une erreur s&apos;est produite. Assurez-vous que QReal est lancé avec les droits d&apos;un superutilisateur</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="104"/>
         <source>Flashing process completed.</source>
-        <translation>La mise à jour du firmware est terminée.</translation>
+        <translation type="vanished">La mise à jour du firmware est terminée.</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="106"/>
         <source>flash.sh not found. Make sure it is present in QReal installation directory</source>
-        <translation>flash.sh n&apos;est pas trouvé. Assurez-vous qu&apos;il soit présent dans le repertoire de  QReal</translation>
+        <translation type="vanished">flash.sh n&apos;est pas trouvé. Assurez-vous qu&apos;il soit présent dans le repertoire de  QReal</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="108"/>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="167"/>
         <source>QReal requires superuser privileges to flash NXT robot</source>
-        <translation>QReal necessite les droits de superutilisateur pour mettre à jour le code du robot NXT</translation>
+        <translation type="vanished">QReal necessite les droits de superutilisateur pour mettre à jour le code du robot NXT</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="116"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="199"/>
         <source>The program has been uploaded</source>
         <translation>Le téléversement du programme est terminé</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="117"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="200"/>
         <source>Do you want to run it?</source>
         <translation>Est-ce que vous voulez l&apos;executer ?</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="126"/>
         <source>NXT not found. Check USB connection and make sure the robot is ON</source>
-        <translation>NXT n&apos;est pas détecté. Vérifez la connexion USB et assurez-vous que le robot ne soit pas éteint</translation>
+        <translation type="vanished">NXT n&apos;est pas détecté. Vérifez la connexion USB et assurez-vous que le robot ne soit pas éteint</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="128"/>
         <source>NXT is not in reset mode. Please reset your NXT manually and try again</source>
-        <translation>NXT n&apos;est pas dans le mode reset. Réinitalisez votre NXT manuellement et recommencez de nouveau</translation>
+        <translation type="vanished">NXT n&apos;est pas dans le mode reset. Réinitalisez votre NXT manuellement et recommencez de nouveau</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="130"/>
         <source>Firmware flash complete!</source>
-        <translation>Le firmware est flashé !</translation>
+        <translation type="vanished">Le firmware est flashé !</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="138"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="206"/>
         <source>Uploading is already running</source>
         <translation>Le téléversement est deja lancé</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="155"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="224"/>
         <source>Uploading program started. Please don&apos;t disconnect robot during the process</source>
         <translation>Le téléversement du programme est commencé. S&apos;il vous plait, ne déconnectéz pas le robot jusqu&apos;à ce que le processus se termine</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="165"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="235"/>
         <source>Uploading failed. Make sure that X-server allows root to run GUI applications</source>
         <translation>Échec de téléversement. Assurez-vous que le serveur X permet de lancer les applications à l&apos;interface graphique</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="201"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="283"/>
         <source>Could not upload program. Make sure the robot is connected and ON</source>
         <translation>Échec de téléversement du programme. Assurez-vous que le robot soit connecté et qu&apos;il ne soit pas éteint</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="203"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="322"/>
+        <source>Error in reading from firmware file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="545"/>
+        <source>Could not find %1. Check your program was compiled and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="554"/>
+        <source>Could not delete old file. Make sure the robot is connected, turned on.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="560"/>
+        <source>Could not upload program. Make sure the robot is connected, turned on and has enough free memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="573"/>
+        <source>Could not close file on brick. Probably connection to NXT lost at the last stage of uploading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="579"/>
         <source>Uploading completed successfully</source>
         <translation>Téléversement réussi</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="206"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="280"/>
         <source>Compilation error occured. Please check your function blocks syntax. If you sure in their validness contact developers</source>
         <translation>Une erreur de compilation s&apos;est produite. Vérifiez le syntaxe à l&apos;intérieur des blocs &quot;fonction&quot;. S&apos;ils sont valides, contactez les developpeurs</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="210"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="108"/>
+        <source>Could not open %1 for reading.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="114"/>
+        <source>Firmware file is too large to fit into NXT brick memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="123"/>
+        <source>Could not write firmware into NXT memory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="129"/>
+        <source>Firmware successfully flashed into robot, but starting it failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="134"/>
+        <source>Flashing process completed successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="140"/>
+        <source>Flashing NXT brick...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="237"/>
+        <source>You need to have superuser privileges to flash NXT robot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtFlashTool.cpp" line="288"/>
         <source>QReal requires superuser privileges to upload programs on NXT robot</source>
         <translation>QReal necessite les droits de superutilisateur pour mettre à jour le programme du robot NXT</translation>
     </message>
@@ -103,37 +165,37 @@
         <translation>Génération (NXT OSEK C)</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="122"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="126"/>
         <source>Generate code</source>
         <translation>Générer le code</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="127"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="131"/>
         <source>Flash robot</source>
         <translation>Flasher le robot</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="132"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="136"/>
         <source>Upload program</source>
         <translation>Téléverser le programme</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="142"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="146"/>
         <source>Generate NXT OSEK code</source>
         <translation>Générer le code NXT OSEK</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="143"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="147"/>
         <source>Upload program to NXT device</source>
         <translation>Téléverser le programme sur le dispositif NXT</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="193"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="197"/>
         <source>flash.sh not found. Make sure it is present in QReal installation directory</source>
         <translation>flash.sh n&apos;est pas trouvé. Assurez-vous qu&apos;il soit présent dans le repertoire de  QReal</translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="204"/>
+        <location filename="../../../../plugins/robots/generators/nxt/nxtOsekCGenerator/nxtOsekCGeneratorPlugin.cpp" line="208"/>
         <source>upload.sh not found. Make sure it is present in QReal installation directory</source>
         <translation>upload.sh n&apos;est pas trouvé. Assurez-vous qu&apos;il soit présent dans le repertoire de  QReal</translation>
     </message>
