0458130b099f93b768b2ed5ff8d3008d51b4f1dd
anastasia143 <semenova.anastasia143@yandex.ru>
2016-05-11 17:42:37 +0300

    Added docs in installer, changed displayed name

--- installer/packages/qreal-db/ru.qreal.root.db/meta/prebuild-common.sh
+++ installer/packages/qreal-db/ru.qreal.root.db/meta/prebuild-common.sh
@@ -7,3 +7,17 @@ cd "$(dirname "$0")"
 mkdir -p $PWD/../data/plugins/tools
 mkdir -p $PWD/../data/plugins/editors
 mkdir -p $PWD/../data/help
+mkdir -p $PWD/../data/sqldrivers/
+
+cp -r  $BIN_DIR/changelog.txt                      $PWD/../data/
+
+cp -r  $BIN_DIR/help/pics                          $PWD/../data/help/
+cp -r  $BIN_DIR/help/index.html                    $PWD/../data/help/
+cp -r  $BIN_DIR/help/interface.html                $PWD/../data/help/
+cp -r  $BIN_DIR/help/lang.html                     $PWD/../data/help/
+cp -r  $BIN_DIR/help/modelling.html                $PWD/../data/help/
+cp -r  $BIN_DIR/help/operations.html               $PWD/../data/help/
+cp -r  $BIN_DIR/help/style.css                     $PWD/../data/help/
+cp -r  $BIN_DIR/help/version.js                    $PWD/../data/help/
+
+cp -r  $BIN_DIR/translations/ru		               $PWD/../data/translations
