9dee5e3595d284a1ec84b8939cffcbc22be26428
iakov <jake.kirilenko@gmail.com>
2017-06-13 23:53:15 +0300

    Mac & Linux (untested) installers for Pioneer

---
+++ installer/packages/trik-studio/ru.qreal.root.pioneer/meta/prebuild-linux-gnu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+cd "$(dirname "$0")"
+
+mkdir -p $PWD/../data/lib/plugins/editors/
+mkdir -p $PWD/../data/lib/plugins/tools/kitPlugins/
+
+cp $BIN_DIR/librobots-pioneer-kit.so 				$PWD/../data/lib/
+cp $BIN_DIR/plugins/editors/libpioneerMetamodel.so 		$PWD/../data/lib/plugins/editors/
+cp $BIN_DIR/plugins/tools/kitPlugins/librobots-pioneer-interpreter.so	$PWD/../data/lib/plugins/tools/kitPlugins/
+cp $BIN_DIR/plugins/tools/kitPlugins/librobots-pioneer-lua-generator.so	$PWD/../data/lib/plugins/tools/kitPlugins/
