ed996932dfae3508a59d032b9941bd30b1472f2c
yurii-litvinov <yurii.litvinov@gmail.com>
2017-06-20 19:09:19 +0300

    Fixed Linux installer

--- installer/packages/trik-studio/ru.qreal.root.pioneer/meta/prebuild-common.sh
+++ installer/packages/trik-studio/ru.qreal.root.pioneer/meta/prebuild-common.sh
@@ -6,3 +6,7 @@ cd "$(dirname "$0")"
 
 mkdir -p $PWD/../data/plugins/editors
 mkdir -p $PWD/../data/plugins/tools/kitPlugins
+
+cp $BIN_DIR/pioneerStart.sh	$PWD/../data/
+cp $BIN_DIR/pioneerUpload.sh	$PWD/../data/
+cp $BIN_DIR/controller.py	$PWD/../data/
--- installer/packages/trik-studio/ru.qreal.root.pioneer/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/ru.qreal.root.pioneer/meta/prebuild-win32.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 cd "$(dirname "$0")"
 
-cp $BIN_DIR/robots-pioneer-kit.dll   $PWD/../data/
+cp $BIN_DIR/robots-pioneer-kit.dll                                    $PWD/../data/
 cp $BIN_DIR/plugins/editors/pioneerMetamodel.dll                      $PWD/../data/plugins/editors/
 cp $BIN_DIR/plugins/tools/kitPlugins/robots-pioneer-interpreter.dll   $PWD/../data/plugins/tools/kitPlugins/
 cp $BIN_DIR/plugins/tools/kitPlugins/robots-pioneer-lua-generator.dll $PWD/../data/plugins/tools/kitPlugins/
