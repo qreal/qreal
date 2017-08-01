79199f8ab04e6f2e76534ec7fb21705bd6a802ea
yurii-litvinov <yurii.litvinov@gmail.com>
2017-08-01 21:24:26 +0300

    Bumped version to a9

--- plugins/robots/interpreters/interpreterCore/src/customizer.cpp
+++ plugins/robots/interpreters/interpreterCore/src/customizer.cpp
@@ -39,7 +39,7 @@ QImage Customizer::applicationLogo() const
 QString Customizer::productVersion() const
 {
 	/// @todo other storage for it?
-	return "3.2.0-α8";
+	return "3.2.0-α9";
 }
 
 QString Customizer::aboutText() const
