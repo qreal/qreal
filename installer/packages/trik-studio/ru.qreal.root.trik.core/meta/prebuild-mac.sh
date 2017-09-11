27d690d773e26616a7ff39e2a1fe2501b191007e
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2017-09-11 17:42:55 +0300

    installer: QtNetwork & QtMultimedia are common for different plugins, moved to trikstudio_kernel

--- installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-mac.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.core/meta/prebuild-mac.sh
@@ -19,9 +19,6 @@ fix_qreal_dependencies "$BUNDLE_CONTENTS/Lib/librobots-trik-generator-base.1.0.0
 fix_qreal_dependencies "$BUNDLE_CONTENTS/Lib/librobots-trik-kit.1.0.0.dylib"
 fix_qreal_dependencies "$BUNDLE_CONTENTS/Lib/librobots-trik-kit-interpreter-common.1.0.0.dylib"
 
-copy_qt_lib QtNetwork
-copy_qt_lib QtMultimedia
-
 mkdir -p "$BUNDLE_CONTENTS/MacOS"
 rsync -a  $BIN_DIR/system.js                                                 "$BUNDLE_CONTENTS/MacOS/"
 
--- installer/packages/trik-studio/ru.qreal.root.trik_studio_kernel/meta/prebuild-win32.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik_studio_kernel/meta/prebuild-win32.sh
@@ -12,3 +12,8 @@ cp    $BIN_DIR/robots-generator-base.dll                             $PWD/../dat
 cp    $BIN_DIR/robots-kit-base.dll                                   $PWD/../data
 cp    $BIN_DIR/robots-utils.dll                                      $PWD/../data
 cp    $BIN_DIR/qextserialport.dll                                    $PWD/../data
+
+
+cp    $QT_DIR/bin/Qt5Network.dll			$PWD/../data
+cp    $QT_DIR/bin/Qt5Multimedia.dll			$PWD/../data
+
