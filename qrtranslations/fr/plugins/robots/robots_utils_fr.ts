0c016969874ad71e196ab7b1b7487ab2228f8d29
greg <lahmatbiy@gmail.com>
2018-02-08 18:03:51 +0300

    more informative error for connection to robot
    Signed-off-by: greg <lahmatbiy@gmail.com>

--- qrtranslations/fr/plugins/robots/robots_utils_fr.ts
+++ qrtranslations/fr/plugins/robots/robots_utils_fr.ts
@@ -21,25 +21,33 @@
     </message>
 </context>
 <context>
+    <name>SensorsGraph</name>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.ui" line="25"/>
+        <source>SensorsGraph</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>trik::UploaderTool</name>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="130"/>
+        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="131"/>
         <source>WinSCP process failed to launch, check path in settings.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="133"/>
-        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="148"/>
+        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="134"/>
+        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="149"/>
         <source>Uploading failed, check connection and try again.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="145"/>
+        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="146"/>
         <source>Uploaded successfully!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="176"/>
+        <location filename="../../../../plugins/robots/utils/src/uploaderTool.cpp" line="177"/>
         <source>%1 is not installed. Please install %1 first.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -62,12 +70,12 @@
 <context>
     <name>utils::robotCommunication::TcpRobotCommunicator</name>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp" line="80"/>
+        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp" line="83"/>
         <source>Empty program name, can not upload</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp" line="88"/>
+        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicator.cpp" line="91"/>
         <source>Can not read generated file, uploading aborted</source>
         <translation type="unfinished"></translation>
     </message>
@@ -75,13 +83,64 @@
 <context>
     <name>utils::robotCommunication::TcpRobotCommunicatorWorker</name>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicatorWorker.cpp" line="186"/>
+        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicatorWorker.cpp" line="229"/>
         <source>Unable to resolve host.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicatorWorker.cpp" line="204"/>
-        <source>Connection failed</source>
+        <location filename="../../../../plugins/robots/utils/src/robotCommunication/tcpRobotCommunicatorWorker.cpp" line="247"/>
+        <source>Connection failed. IP: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>utils::sensorsGraph::SensorViewer</name>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorViewer.cpp" line="132"/>
+        <source>Save values history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorViewer.cpp" line="132"/>
+        <source>Comma-Separated Values Files (*.csv)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorViewer.cpp" line="238"/>
+        <source>value: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>utils::sensorsGraph::SensorsGraph</name>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="149"/>
+        <source>Stop tracking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="153"/>
+        <source>Start tracking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="157"/>
+        <source>Reset plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="161"/>
+        <source>Zoom In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="165"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../plugins/robots/utils/src/graphicsWatcher/sensorsGraph.cpp" line="169"/>
+        <source>Export values...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
