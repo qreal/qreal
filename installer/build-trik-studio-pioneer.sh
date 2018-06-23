f76f57cb4aee0567ed2e8633f58280e653e14686
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-06-23 14:24:51 +0300

    trik-runtime 3.3.0 update

--- installer/build-trik-studio-pioneer.sh
+++ installer/build-trik-studio-pioneer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Usage: build-trik-studio.sh <path-to-qt> <path-to-qt-installer-framework> [tag]. See build-installer.sh for more info.
 
-set -o nounset
-set -o errexit
+set -euo pipefail
 
 cd "$(dirname "$0")"
 
@@ -10,5 +9,5 @@ export QREAL_BUILD_TAG=${3:-}
 
 [ $OSTYPE == "linux-gnu" ] && EXCLUDE="-e ru.qreal.root.associations" || EXCLUDE=
 grep -q "darwin" <<< $OSTYPE && EXCLUDE="-e ru.qreal.root.associations" || :
-grep -q "darwin" <<< $OSTYPE && export PRODUCT_DISPLAYED_NAME="TRIK Studio" || :
-chmod +x $PWD/build-installer.sh && $PWD/build-installer.sh $1 $2 trik-studio-pioneer $EXCLUDE
+grep -q "darwin" <<< $OSTYPE && export PRODUCT_DISPLAYED_NAME="TRIK Studio PIONEER" || :
+chmod +x $PWD/build-installer.sh && $PWD/build-installer.sh $1 $2 trik-studio $EXCLUDE
