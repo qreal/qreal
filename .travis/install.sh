352eda20dae4efbd6b0ff48a33a13f75ad40a255
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-08-18 09:07:45 +0300

    CI: move docker pull to before_install

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,15 +2,9 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    brew update
     brew install qt python@2 ccache
     ;;
   linux)
-    docker pull trikset/linux-builder
-    if [[ "$CONFIG" == "debug" ]]
-    then
-      pip install --user codecov
-    fi
     ;;
   *) exit 1 ;;
 esac
