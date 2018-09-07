083acad493dc9c79fbbe029b35c120046bbff5a7
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-09-07 16:36:12 +0300

    TravisCI: set CCACHE custom dir

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,22 +4,22 @@ case $TRAVIS_OS_NAME in
   osx)
      export PATH="/usr/local/opt/qt/bin:$PATH"
      export PATH="/usr/local/opt/ccache/libexec:$PATH"
-     EXECUTOR=time
+     EXECUTOR=
     ;;
   linux)
      EXECUTOR="time docker exec builder "
    ;;
   *) exit 1 ;;
 esac
-$EXECUTOR export CCACHE_DIR=$HOME/.ccache/$TRAVIS_OS_NAME-$CONFIG
-$EXECUTOR which g++
-$EXECUTOR g++ --version
-$EXECUTOR ccache -M 0
-$EXECUTOR pkg-config --list-all
-if [ "$VERA" = "true" ]; then $EXECUTOR .travis/runVera++.sh ; fi
-$EXECUTOR qmake -Wall CONFIG+=$CONFIG CONFIG+=no-sanitizers $PROJECT.pro
-$EXECUTOR sh -c 'make -k -j2 qmake_all 1>>build.log 2>&1'
-$EXECUTOR sh -c 'make -k -j2 all 1>>build.log 2>&1'
-$EXECUTOR ccache -sp
-$EXECUTOR sh -c "cd bin/$CONFIG && ls"
-$EXECUTOR sh -c "export DISPLAY=:0 && cd bin/$CONFIG && $TESTS"
+$EXECUTOR sh -c "export CCACHE_DIR=$HOME/.ccache/$TRAVIS_OS_NAME-$CONFIG \
+&& which g++ \
+&& g++ --version \
+&& ccache -M 0 \
+&& pkg-config --list-all \
+&& if $VERA ; then .travis/runVera++.sh ; fi \
+&& qmake -Wall CONFIG+=$CONFIG CONFIG+=no-sanitizers $PROJECT.pro \
+&& sh -c 'make -k -j2 qmake_all 1>>build.log 2>&1' \
+&& sh -c 'make -k -j2 all 1>>build.log 2>&1' \
+&& ccache -sp \
+&& sh -c \"cd bin/$CONFIG && ls\" \
+&& sh -c \"export DISPLAY=:0 && cd bin/$CONFIG && $TESTS\""
