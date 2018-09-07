f9eebfa94fed11fb0e1012a22b7547fba11c0090
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-09-07 23:43:14 +0300

    TravisCI: force brew link for Qt & ccache

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
     brew install qt python@2 ccache
+    brew link --force ccache
+    brew link --force qt
     ;;
   linux)
     docker run -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
