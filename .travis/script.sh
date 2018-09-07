b101fc555a71ea2dcace25958980be6e79aa9d94
Iakov 'Jake' Kirilenko <Jake.Kirilenko@gmail.com>
2018-09-07 17:15:25 +0300

    TavisCI: cache pip data to speedup

--- .travis/script.sh
+++ .travis/script.sh
@@ -17,9 +17,9 @@ $EXECUTOR sh -c "export CCACHE_DIR=$HOME/.ccache/$TRAVIS_OS_NAME-$CONFIG \
 && ccache -M 0 \
 && pkg-config --list-all \
 && if $VERA ; then .travis/runVera++.sh ; fi \
-&& qmake -Wall CONFIG+=$CONFIG CONFIG+=no-sanitizers $PROJECT.pro \
+&& qmake -Wall CONFIG+=$CONFIG CONFIG+=no-sanitizers QMAKE_CXX=\"ccache $CXX\" $PROJECT.pro \
 && sh -c 'make -k -j2 qmake_all 1>>build.log 2>&1' \
 && sh -c 'make -k -j2 all 1>>build.log 2>&1' \
-&& ccache -sp \
+&& ccache -s \
 && sh -c \"cd bin/$CONFIG && ls\" \
 && sh -c \"export DISPLAY=:0 && cd bin/$CONFIG && $TESTS\""
