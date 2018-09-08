e753c33c44086be8a190b5be55fb630e10062329
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-09-08 10:49:57 +0300

    TravisCI: no 'brew update' to speedup OS X builds

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    brew update
+#    brew update
     ;;
   linux)
     docker pull trikset/linux-builder
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,9 +2,11 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    brew install qt python@2 ccache
-    brew link --force ccache
-    brew link --force qt
+    rmdir $CELLAR_CACHE_DIR/{qt,ccache} && brew install qt ccache \
+    ||  { \
+          brew link --force ccache ;  \
+          brew link --force qt ; \
+        }
     ;;
   linux)
     docker run -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
