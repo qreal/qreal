3a7b47b3412ff498a5312eb4ffdbd1a3a236c3a9
greg <lahmatbiy@gmail.com>
2018-10-17 14:51:43 +0300

    fix gamepad installer sh for linux-gnu and tools-installer

--- installer/packages/trik-studio/ru.qreal.root.trik.gamepad/meta/prebuild-linux-gnu.sh
+++ installer/packages/trik-studio/ru.qreal.root.trik.gamepad/meta/prebuild-linux-gnu.sh
@@ -9,11 +9,11 @@ mkdir -p $PWD/../data/lib/
 cp -pr $QT_DIR/lib/libQt5MultimediaWidgets.so*		$PWD/../data/lib/
 cp -pr $QT_DIR/lib/libQt5OpenGL.so*					$PWD/../data/lib/
 
-cd gamepad-build
+cd $PWD/../data/gamepad-build
 $QT_DIR/bin/qmake -nocache CONFIG+=release
 make -j4
 
 cd ..
-rm -rf ../data/gamepad
-mv gamepad-build/gamepad ../data/
+mkdir bin
+mv gamepad-build/gamepad bin/gamepad
 rm -rf gamepad-build
