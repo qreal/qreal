c5b3ae7a3d2f929156680d6e680e0c6ac0cc0f07
greg <lahmatbiy@gmail.com>
2018-11-22 14:23:11 +0300

    change travis scripts (raw)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,6 +3,10 @@ set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
 #    brew update
+    if [[ "$CONFIG" == "debug" ]]
+    then
+      python3.6 -m pip install --user codecov
+    fi
     ;;
   linux)
     docker pull trikset/linux-builder
--- .travis/install.sh
+++ .travis/install.sh
@@ -2,11 +2,14 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    rmdir $CELLAR_CACHE_DIR/{qt,ccache} && brew install qt ccache \
-    ||  { \
-          brew link --force ccache ;  \
-          brew link --force qt ; \
-        }
+    REQURED_PACKAGES="qt ccache https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb"
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    for pkg in $REQURED_PACKAGES ; do
+      p="${pkg##*/}"
+      p="${p%.*}"
+      rmdir $CELLAR_CACHE_DIR/$p && brew install $pkg \
+      || { brew unlink $p ; brew link --force $p ; }
+    done
     ;;
   linux)
     docker run -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
