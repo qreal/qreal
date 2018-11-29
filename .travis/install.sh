36c8d1760865a9f8bd16c3a3803935428d5a63d5
Iakov Kirilenko <jake.kirilenko@gmail.com>
2018-11-29 14:24:01 +0300

    Python 3.5 from pyenv

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,13 +2,24 @@
 set -euxo pipefail
 case $TRAVIS_OS_NAME in
   osx)
-    rmdir $CELLAR_CACHE_DIR/{qt,ccache} && brew install qt ccache \
-    ||  { \
-          brew link --force ccache ;  \
-          brew link --force qt ; \
-        }
+    REQUIRED_PACKAGES="qt ccache pyenv"
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    for pkg in $REQUIRED_PACKAGES ; do
+      p="${pkg##*/}"
+      p="${p%.*}"
+      rmdir $CELLAR_CACHE_DIR/$p && brew install $pkg \
+      || { brew unlink $p ; brew link --force $p ; }
+    done
+    brew update && brew upgrade $REQUIRED_PACKAGES
+    # export PYENV_ROOT="$CELLAR_CACHE_DIR/.pyenv"
+    export PATH="$(pyenv root)/bin:$PATH"
+    eval "$(pyenv init -)"
+    #pyenv update || true
+    pyenv install --list
+    env CFLAGS="-O2 -fPIC" pyenv install -s 3.5.6
     ;;
   linux)
+    docker pull trikset/linux-builder
     docker run -d -v $HOME:$HOME:rw -w `pwd` --name builder trikset/linux-builder Xvfb :0
     ;;
   *) exit 1 ;;
